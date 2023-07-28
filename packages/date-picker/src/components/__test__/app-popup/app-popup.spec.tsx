81e4a289077674084ad19d949e96cb3aa7ad9c4a
Tom Österlund <tom.osterlund1@gmail.com>
2023-07-28 23:11:03 +0200

    refactor tests

--- packages/date-picker/src/components/__test__/app-popup/app-popup.spec.tsx
+++ packages/date-picker/src/components/__test__/app-popup/app-popup.spec.tsx
@@ -3,61 +3,46 @@ import {
   it,
   expect,
   beforeEach,
-} from '../../../../../shared/utils/stateless/testing/unit/unit-testing-library.impl'
+} from '../../../../../../shared/utils/stateless/testing/unit/unit-testing-library.impl'
 import { cleanup, render, screen, waitFor } from '@testing-library/preact'
-import AppPopup from '../app-popup'
-import { MONTH_VIEW } from '../../constants/test-ids'
-import { __createDatePickerAppSingleton__ } from '../../../../../shared/utils/stateless/testing/unit/factories/create-date-picker-app-singleton'
-import { AppContext } from '../../utils/stateful/app-context'
+import AppPopup from '../../app-popup'
+import { MONTH_VIEW } from '../../../constants/test-ids'
+import { __createDatePickerAppSingleton__ } from '../../../../../../shared/utils/stateless/testing/unit/factories/create-date-picker-app-singleton'
+import { AppContext } from '../../../utils/stateful/app-context'
+import { factory } from "./utils";
 
 describe('AppPopup', () => {
-  const $app = __createDatePickerAppSingleton__(undefined, 'en-US')
-
   beforeEach(() => {
     cleanup()
   })
 
   it('should render month view as default view', () => {
-    render(
-      <AppContext.Provider value={$app}>
-        <AppPopup />
-      </AppContext.Provider>
-    )
-
+    factory()
     expect(screen.queryByTestId(MONTH_VIEW)).not.toBeNull()
   })
 
   it('should display years view', async () => {
-    const { container } = render(
-      <AppContext.Provider value={$app}>
-        <AppPopup />
-      </AppContext.Provider>
-    )
-
+    const container = factory()
     const toggleViewElement = container.querySelector(
       '.sx__date-picker__month-view-header__month-year'
     )
-    if (!toggleViewElement) throw new Error('monthYear is null')
+    if (!toggleViewElement) throw new Error('no toggle view element was found')
+
     toggleViewElement.dispatchEvent(new MouseEvent('click', { bubbles: true }))
 
     const yearsView = await screen.findByTestId('years-view')
-
     await waitFor(() => {
       expect(yearsView).not.toBeNull()
     })
   })
 
   it('should toggle from years to month view', async () => {
-    const { container } = render(
-      <AppContext.Provider value={$app}>
-        <AppPopup />
-      </AppContext.Provider>
-    )
-
+    const container = factory()
     const toggleViewElement = container.querySelector(
       '.sx__date-picker__month-view-header__month-year'
     )
-    if (!toggleViewElement) throw new Error('monthYear is null')
+    if (!toggleViewElement) throw new Error('no toggle view element was found')
+
     toggleViewElement.dispatchEvent(new MouseEvent('click', { bubbles: true }))
 
     const yearsView = await screen.findByTestId('years-view')
