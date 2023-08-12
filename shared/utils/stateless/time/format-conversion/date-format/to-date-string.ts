9f152793e476d6c46ef4d91b62795ba0ccd1021b
Tom Österlund <tom.osterlund1@gmail.com>
2023-08-12 18:16:08 +0200

    refactor

--- shared/utils/stateless/time/format-conversion/date-format/to-date-string.ts
+++ shared/utils/stateless/time/format-conversion/date-format/to-date-string.ts
@@ -7,6 +7,16 @@ import { doubleDigit } from '../../date-time-mutation/date-time-mutation.ts'
 import { LocaleNotSupportedError } from '../../../errors/locale-not-supported.error.ts'
 import { InvalidDateFormatError } from '../../../errors/invalid-date-format.error.ts'
 
+const _getMatchesOrThrow = (
+  format: string,
+  matcher: RegExp,
+  locale: string
+) => {
+  const matches = format.match(matcher)
+  if (!matches) throw new InvalidDateFormatError(format, locale)
+  return matches
+}
+
 export const toDateString = (format: string, locale: string) => {
   const internationalFormat = /^\d{4}-\d{2}-\d{2}$/
   if (internationalFormat.test(format)) return format // allow international format regardless of locale
@@ -19,25 +29,19 @@ export const toDateString = (format: string, locale: string) => {
   const pattern224Dotted = /^(\d{1,2})\.(\d{1,2})\.(\d{4})$/
 
   if (order === Order.DMY && delimiter === Delimiter.SLASH) {
-    const matches = format.match(pattern224Slashed)
-    if (!matches) throw new InvalidDateFormatError(format, locale)
-
+    const matches = _getMatchesOrThrow(format, pattern224Slashed, locale)
     const [, day, month, year] = matches
     return `${year}-${doubleDigit(+month)}-${doubleDigit(+day)}`
   }
 
   if (order === Order.MDY && delimiter === Delimiter.SLASH) {
-    const matches = format.match(pattern224Slashed)
-    if (!matches) throw new InvalidDateFormatError(format, locale)
-
+    const matches = _getMatchesOrThrow(format, pattern224Slashed, locale)
     const [, month, day, year] = matches
     return `${year}-${doubleDigit(+month)}-${doubleDigit(+day)}`
   }
 
   if (order === Order.DMY && delimiter === Delimiter.PERIOD) {
-    const matches = format.match(pattern224Dotted)
-    if (!matches) throw new InvalidDateFormatError(format, locale)
-
+    const matches = _getMatchesOrThrow(format, pattern224Dotted, locale)
     const [, day, month, year] = matches
     return `${year}-${doubleDigit(+month)}-${doubleDigit(+day)}`
   }
