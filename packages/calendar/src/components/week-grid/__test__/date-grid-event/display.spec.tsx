0ac8c35a876f70bd412ed47ce9e4540beec5c86f
Tom Österlund <tom.osterlund1@gmail.com>
2023-10-03 18:12:58 +0200

    test(calendar): add unit tests for display style of date-grid-event

---
+++ packages/calendar/src/components/week-grid/__test__/date-grid-event/display.spec.tsx
@@ -0,0 +1,64 @@
+import {
+  describe,
+  expect,
+  it,
+} from '@schedule-x/shared/src/utils/stateless/testing/unit/unit-testing-library.impl'
+import { createCalendarAppSingleton } from '../../../../factory'
+import { cleanup, screen, waitFor } from '@testing-library/preact'
+import { afterEach } from 'vitest'
+import { factory } from './utils'
+import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop/src'
+
+describe('display value of date grid event element', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  const selectedDate = '2024-10-01'
+  const oneDayEventId = 'my-event-id'
+  const oneDayEventTitle = 'My event'
+
+  const $app = createCalendarAppSingleton({
+    datePicker: {
+      selectedDate: selectedDate,
+    },
+    plugins: [createDragAndDropPlugin()],
+    events: [
+      {
+        id: oneDayEventId,
+        title: oneDayEventTitle,
+        time: {
+          start: selectedDate,
+          end: selectedDate,
+        },
+      },
+    ],
+  })
+  $app.elements.calendarWrapper = document.createElement('div')
+  const day = document.createElement('div')
+  day.classList.add('sx__time-grid-day')
+  $app.elements.calendarWrapper.appendChild(day)
+
+  describe('event when not in drag state', () => {
+    it('should have style "display: flex"', () => {
+      factory($app, oneDayEventId, 1)
+
+      const oneDayEvent = screen.getByText(oneDayEventTitle)
+      expect(oneDayEvent.style.display).toBe('flex')
+    })
+  })
+
+  describe('event when in drag state', () => {
+    it('should have style "display: none"', async () => {
+      factory($app, oneDayEventId, 1)
+      const oneDayEvent = screen.getByText(oneDayEventTitle)
+
+      const mouseDownEvent = new MouseEvent('mousedown', {})
+      oneDayEvent.dispatchEvent(mouseDownEvent)
+
+      await waitFor(() => {
+        expect(oneDayEvent.style.display).toBe('none')
+      })
+    })
+  })
+})
