1082b36c80104b3d93f704ba4f813953ff478aee
Tom Österlund <tom.osterlund1@gmail.com>
2023-10-09 08:47:26 +0200

    fix: show only one day in day mode with hybrid day boundaries

--- packages/calendar/src/utils/stateless/views/week/create-week-day-contexts.ts
+++ packages/calendar/src/utils/stateless/views/week/create-week-day-contexts.ts
@@ -3,26 +3,28 @@ import { toDateString } from '@schedule-x/shared/src/utils/stateless/time/format
 import { WeekDayContexts } from '../../../../types/week-day-context'
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
 import { DateRange } from '../../../../types/date-range'
-import { dateFromDateTime } from '@schedule-x/shared/src/utils/stateless/time/format-conversion/string-to-string'
+import { InternalViewName } from '../../../../enums/internal-view.enum'
+
+const createContextForOneDate = (acc: WeekDayContexts, date: Date) => {
+  const dateString = toDateString(date)
+  acc[dateString] = {
+    date: dateString,
+    timeGridEvents: [],
+    dateGridEvents: {},
+  }
+
+  return acc
+}
 
 export const createWeekDayContexts = ($app: CalendarAppSingleton) => {
+  if ($app.calendarState.view.value === InternalViewName.Day)
+    return createContextForOneDate(
+      {},
+      toJSDate($app.calendarState.range.value!.start)
+    )
+
+  // Week mode
   return $app.timeUnitsImpl
     .getWeekFor(toJSDate(($app.calendarState.range.value as DateRange).start))
-    .filter((date) => {
-      const dateString = toDateString(date)
-      return (
-        dateString >= dateFromDateTime($app.calendarState.range.value!.start) &&
-        dateString <= dateFromDateTime($app.calendarState.range.value!.end)
-      )
-    })
-    .reduce((acc, date) => {
-      const dateString = toDateString(date)
-      acc[dateString] = {
-        date: dateString,
-        timeGridEvents: [],
-        dateGridEvents: {},
-      }
-
-      return acc
-    }, {} as WeekDayContexts)
+    .reduce(createContextForOneDate, {} as WeekDayContexts)
 }
