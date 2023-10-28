970184352e070d2e2a3f08e2ddcab10fd76371cb
Tom Österlund <tom.osterlund1@gmail.com>
2023-10-28 19:01:38 +0200

    feat(): change view automatically when changing screen size

--- packages/calendar/src/utils/stateless/dom/handle-window-resize.ts
+++ packages/calendar/src/utils/stateless/dom/handle-window-resize.ts
@@ -1,4 +1,31 @@
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
+import { View } from '../../../types/view'
+
+const setScreenSizeCompatibleView = (
+  $app: CalendarAppSingleton,
+  isSmall: boolean
+) => {
+  const currentView = $app.config.views.find(
+    (view) => view.name === $app.calendarState.view.value
+  ) as View
+  if (isSmall) {
+    if (currentView.hasSmallScreenCompat) return
+
+    const smallScreenCompatibleView = $app.config.views.find(
+      (view) => view.hasSmallScreenCompat
+    )
+    if (smallScreenCompatibleView)
+      $app.calendarState.view.value = smallScreenCompatibleView.name
+  } else {
+    if (currentView.hasWideScreenCompat) return
+
+    const wideScreenCompatibleView = $app.config.views.find(
+      (view) => view.hasWideScreenCompat
+    )
+    if (wideScreenCompatibleView)
+      $app.calendarState.view.value = wideScreenCompatibleView.name
+  }
+}
 
 export const handleWindowResize = ($app: CalendarAppSingleton) => {
   const documentRoot = document.documentElement
@@ -12,7 +39,11 @@ export const handleWindowResize = ($app: CalendarAppSingleton) => {
 
   if (!calendarRoot) return
 
-  $app.calendarState.isSmallScreen.value =
-    calendarRoot.clientWidth < smallCalendarBreakpoint
-  console.log($app.calendarState.isSmallScreen.value)
+  const isSmall = calendarRoot.clientWidth < smallCalendarBreakpoint
+  const didIsSmallScreenChange =
+    isSmall !== $app.calendarState.isSmallScreen.value
+  if (!didIsSmallScreenChange) return
+
+  $app.calendarState.isSmallScreen.value = isSmall
+  setScreenSizeCompatibleView($app, isSmall)
 }
