51e1c2b7ae70ec907ddcfb70df0d95b5ac34e5df
Tom Österlund <tom.osterlund1@gmail.com>
2023-11-14 14:15:08 +0100

    chore: fix lint findings

--- packages/calendar/src/utils/stateful/hooks/use-wrapper-classes.tsx
+++ packages/calendar/src/utils/stateful/hooks/use-wrapper-classes.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'preact/compat'
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
-import { handleWindowResize } from '../../stateless/dom/handle-window-resize'
 
 export default function useWrapperClasses($app: CalendarAppSingleton) {
   const calendarWrapperClass = 'sx__calendar-wrapper'
