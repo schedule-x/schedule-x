c9d4c6b3faa2187991d627a01f45c1ac7b53aebc
Tom Österlund <tom.osterlund1@gmail.com>
2023-11-14 14:12:14 +0100

    test(): clean up after unit test

--- packages/calendar/src/components/__test__/calendar-wrapper.spec.tsx
+++ packages/calendar/src/components/__test__/calendar-wrapper.spec.tsx
@@ -4,9 +4,10 @@ import {
   expect,
 } from '@schedule-x/shared/src/utils/stateless/testing/unit/unit-testing-library.impl'
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
-import { render, waitFor } from '@testing-library/preact'
+import { cleanup, render, waitFor } from '@testing-library/preact'
 import CalendarWrapper from '../calendar-wrapper'
 import { __createAppWithViews__ } from '../../utils/stateless/testing/__create-app-with-views__'
+import { afterEach } from 'vitest'
 
 const renderComponent = ($app: CalendarAppSingleton) => {
   render(<CalendarWrapper $app={$app} />)
@@ -16,6 +17,10 @@ const CALENDAR_WRAPPER_SELECTOR = '.sx__calendar-wrapper'
 const SMALL_CALENDAR_CLASS = 'sx__is-calendar-small'
 
 describe('CalendarWrapper', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
   describe('when the calendar is small (less than 700px)', () => {
     it('should have an element class of sx__is-calendar-small', async () => {
       const $app = __createAppWithViews__()
