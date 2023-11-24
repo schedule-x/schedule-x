f6f9dca3c8392e824e749cfe74c51a5b30ef3500
Tom Österlund <tom.osterlund1@gmail.com>
2023-11-24 17:29:23 +0100

    refactor: use separate vite config for building e2e assets

---
+++ packages/shared/src/config/e2e.vite.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve('index.html'),
+        datePicker001: resolve('cypress/pages/date-picker/001-navigate-months.html'),
+        dragAndDrop001: resolve('cypress/pages/calendar/001-drag-and-drop.html'),
+      },
+    },
+  },
+})
