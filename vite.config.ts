07bec2ab018e7e8df5ca8f3cc6d4b80585ca1c91
Tom Österlund <tom.osterlund1@gmail.com>
2023-11-25 15:47:55 +0100

    refactor: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
