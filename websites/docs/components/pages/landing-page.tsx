9b4f6b7595077955447fa27104c9b2535cadbbe8
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-01 21:31:52 +0100

    docs: create background image for landing page

--- websites/docs/components/pages/landing-page.tsx
+++ websites/docs/components/pages/landing-page.tsx
@@ -1,7 +1,17 @@
+import styles from './landing-page.module.css'
+import '@schedule-x/theme-default/dist/index.css'
+import Image from 'next/image'
+
 export default function LandingPage() {
+
   return <>
-    <h1>
-      hello tom
-    </h1>
+    <div className={styles.landingPage}>
+      <div className={styles.landingPageBG}></div>
+
+      <h1>
+        <span className={styles.headingGradient}>Material design</span> scheduling components for the web
+      </h1>
+
+    </div>
   </>
 }
