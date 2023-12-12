d87c0f6dccde30737cfff1bd6768e88a8fce6bb7
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-12 18:34:37 +0100

    docs: add page for general configuration of calendar

--- development/calendar/main.tsx
+++ development/calendar/main.tsx
@@ -21,6 +21,7 @@ import { seededEvents } from '../data/seeded-events.ts'
 const calendarElement = document.getElementById('calendar') as HTMLElement
 
 const calendar = createCalendar(calendarElement, {
+  firstDayOfWeek: 0,
   // locale: 'de-DE',
   locale: 'en-US',
   // locale: 'zh-CN',
