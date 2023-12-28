4f093c74068986643aae3db1f4d1dcfb6848c5d7
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-28 18:13:33 +0100

    chore: format

--- website/components/pages/landing-page.tsx
+++ website/components/pages/landing-page.tsx
@@ -19,7 +19,10 @@ export default function LandingPage() {
           and date picker
         </h1>
 
-        <h2>React, Vue and plain JS components that make scheduling simple and elegant in your web project</h2>
+        <h2>
+          React, Vue and plain JS components that make scheduling simple and
+          elegant in your web project
+        </h2>
 
         <div className={'landingPageActions'}>
           <Link href={'/demos/calendar'}>
--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -11,7 +11,8 @@ const config: DocsThemeConfig = {
   // chat: {
   //   link: 'https://discord.com',
   // },
-  docsRepositoryBase: 'https://github.com/schedule-x/schedule-x/tree/main/website',
+  docsRepositoryBase:
+    'https://github.com/schedule-x/schedule-x/tree/main/website',
   footer: {
     text: `© 2023-present Tom Österlund`,
   },
@@ -27,11 +28,25 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <link rel="shortcut icon" href="/images/favicon_io/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_io/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_io/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_io/favicon-16x16.png"/>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/favicon_io/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon_io/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon_io/favicon-16x16.png"
+      />
     </>
-  )
+  ),
 }
 
 export default config
