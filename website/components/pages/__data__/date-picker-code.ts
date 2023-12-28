68f0029a0e1cb6bee262c153b8879250279a6f9b
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-28 19:54:11 +0100

    docs: add date picker docs

--- website/components/pages/__data__/date-picker-code.ts
+++ website/components/pages/__data__/date-picker-code.ts
@@ -2,7 +2,7 @@ export const datePickerCode = `import { createDatePicker } from '@schedule-x/dat
 import '@schedule-x/theme-default/dist/index.css'
 
 const datePickerEl = document.getElementById('date-picker') as HTMLElement
-const datePicker = createDatePicker(datePickerEl, {
+const datePicker = createDatePicker({
   listeners: {
     onChange: (date) => {
       console.log('date changed', date)
