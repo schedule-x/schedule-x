56655b94d0b68a1431ae966c1d93601926d19db6
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-29 21:14:33 +0100

    fix: make icon color in event modal reactive to theme changes

---
+++ packages/event-modal/src/utils/stateful/use-icon-colors.ts
@@ -0,0 +1,15 @@
+import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
+import { effect, signal } from '@preact/signals'
+
+export const useIconColors = ($app: CalendarAppSingleton) => {
+  const ICON_COLOR_LIGHT_MODE = '#000'
+  const ICON_COLOR_DARK_MODE = 'var(--sx-color-neutral-variant)'
+  const iconColor = signal(ICON_COLOR_LIGHT_MODE)
+  if ($app.calendarState.isDark.value) iconColor.value = ICON_COLOR_DARK_MODE
+
+  effect(() => {
+    if ($app.calendarState.isDark.value) iconColor.value = ICON_COLOR_DARK_MODE
+    else iconColor.value = ICON_COLOR_LIGHT_MODE
+  })
+  return iconColor
+}
