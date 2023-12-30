caad5e3b5188058e7480448ef18fd06a66c1005f
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-30 07:40:46 +0100

    docs: add og url

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -25,35 +25,43 @@ const config: DocsThemeConfig = {
     }
   },
 
-  head: (
-    <>
-      <link rel="shortcut icon" href="/images/favicon_io/favicon.ico" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/images/favicon_io/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/images/favicon_io/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/images/favicon_io/favicon-16x16.png"
-      />
-      <meta
-        property="og:image"
-        content="https://schedule-x.s3.eu-west-1.amazonaws.com/logo_open_graph.png"
-      />
-      <meta property="og:image:width" content="1600" />
-      <meta property="og:image:height" content="630" />
-      <meta property="twitter:card" content="summary_large_image" />
-    </>
-  ),
+  head: () => {
+    // using useRouter, get full current path including host
+    const { asPath, basePath } = useRouter()
+    const url = `https://schedule-x.dev${basePath}${asPath}`
+
+    return (
+      <>
+        <link rel="shortcut icon" href="/images/favicon_io/favicon.ico"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon_io/favicon-16x16.png"
+        />
+        <meta
+          property="og:image"
+          content="https://schedule-x.s3.eu-west-1.amazonaws.com/logo_open_graph.png"
+        />
+        <meta property="og:image:width" content="1600"/>
+        <meta property="og:image:height" content="630"/>
+        <meta property="og:url" content={url} />
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="og:locale" content="en_US"/>
+      </>
+    )
+  }
 }
 
 export default config
