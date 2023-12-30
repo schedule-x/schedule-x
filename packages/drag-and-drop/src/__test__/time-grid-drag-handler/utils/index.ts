af5c82e1058e9f5dbc067623a95b055b5a44fa5a
Tom Österlund <tom.osterlund1@gmail.com>
2023-12-30 16:20:23 +0100

    test: add test cases for drag and drop in time grid with custom day boundaries

--- packages/drag-and-drop/src/__test__/time-grid-drag-handler/utils/index.ts
+++ packages/drag-and-drop/src/__test__/time-grid-drag-handler/utils/index.ts
@@ -1,10 +1,10 @@
 export const dragEventNQuarters = (
   clickEvent: MouseEvent,
   nStepsToDrag: number,
-  direction: 'up' | 'down'
+  direction: 'up' | 'down',
+  pixelPerQuarterHour = 16.6666666667
 ) => {
   let currentY = clickEvent.clientY
-  const pixelPerQuarterHour = 16.6666666667
   for (let i = 0; i < nStepsToDrag; i++) {
     if (direction === 'down') {
       currentY += pixelPerQuarterHour
@@ -18,4 +18,12 @@ export const dragEventNQuarters = (
     } as MouseEvent
     document.dispatchEvent(new MouseEvent('mousemove', event))
   }
+}
+
+export const dragEventNQuarters12HourGrid = (
+  clickEvent: MouseEvent,
+  nStepsToDrag: number,
+  direction: 'up' | 'down'
+) => {
+  dragEventNQuarters(clickEvent, nStepsToDrag, direction, 16.6666666667 * 2)
 }
