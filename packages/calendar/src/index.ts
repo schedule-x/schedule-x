c83d873605cdd23d1878bcc3274dd1104c3397bf
Tom Österlund <tom.osterlund1@gmail.com>
2024-01-02 21:51:48 +0100

    feat: expose types and functions from shared package

--- packages/calendar/src/index.ts
+++ packages/calendar/src/index.ts
@@ -7,19 +7,10 @@ import {
   CalendarConfigExternal as CalendarConfig,
   CustomComponentFn,
 } from '@schedule-x/shared/src/interfaces/calendar/calendar-config'
-import { View } from '@schedule-x/shared/src/types/calendar/view'
 import CalendarApp from './calendar.app'
 import { createPreactView } from './utils/stateful/preact-view/preact-view'
-import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
-import { ViewConfig } from '@schedule-x/shared/src/types/calendar/view'
 
-export type {
-  CalendarConfig,
-  CustomComponentFn,
-  View,
-  CalendarAppSingleton,
-  ViewConfig,
-}
+export type { CalendarConfig, CustomComponentFn }
 
 export {
   createCalendar,
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,8 @@
-export default {}
+import CalendarAppSingleton from './interfaces/calendar/calendar-app-singleton'
+
+export type { View, ViewConfig } from './types/calendar/view'
+export type { CalendarAppSingleton }
+export {
+  addDays,
+  addMonths,
+} from './utils/stateless/time/date-time-mutation/adding'
