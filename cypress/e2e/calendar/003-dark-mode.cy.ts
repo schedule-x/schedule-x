0db46c85010bfc294aafc1513a45b7e2839c2ed2
Tom Österlund <tom.osterlund1@gmail.com>
2024-01-03 07:14:09 +0100

    test: record new screenshots

--- cypress/e2e/calendar/003-dark-mode.cy.ts
+++ cypress/e2e/calendar/003-dark-mode.cy.ts
@@ -11,6 +11,7 @@ describe('Calendar dark mode', () => {
   })
 
   it('should render events in the week view', () => {
+    cy.wait(1000) // for scrolling to finish
     cy.compareSnapshot(
       'calendar-dark-mode__week-view',
       SNAPSHOT_FAULT_TOLERANCE
@@ -26,6 +27,7 @@ describe('Calendar dark mode', () => {
   })
 
   it('should render events in the day view', () => {
+    cy.wait(1000) // for scrolling to finish
     calendarHeader.openViewByLabel('Day')
     cy.compareSnapshot('calendar-dark-mode__day-view', SNAPSHOT_FAULT_TOLERANCE)
   })
