391ab3f77b6fa36234813e5981ed911df6489253
ArtBook <artenio.capaldo@gmail.com>
2024-01-05 14:55:14 +0100

    locale_it implementation

---
+++ packages/translations/src/locales/it-IT/date-picker.ts
@@ -0,0 +1,8 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerItIT: DatePickerTranslations = {
+  Date: 'Data',
+  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'Next month': 'Mese successivo',
+  'Previous month': 'Mese precedente',
+}
---
+++ packages/translations/src/locales/it-IT/index.ts
@@ -0,0 +1,8 @@
+import { datePickerItIT } from './date-picker'
+import { Language } from '../../types/language.translations'
+import { calendarItIT } from './calendar'
+
+export const itIT: Language = {
+  ...datePickerItIT,
+  ...calendarItIT,
+}
