fe5ced18366abffe37426da3afc73c3b7c25f5e7
Tom Österlund <tom.osterlund1@gmail.com>
2024-01-08 06:39:26 +0100

    fix: remove unwanted scroll after drag & drop

--- packages/scroll-controller/src/scroll-controller.plugin.ts
+++ packages/scroll-controller/src/scroll-controller.plugin.ts
@@ -3,11 +3,14 @@ import { PluginName } from '@schedule-x/shared/src/enums/plugin-name.enum'
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
 import ScrollControllerConfig from './interfaces/config'
 import { timePointsFromString } from '@schedule-x/shared/src/utils/stateless/time/time-points/string-conversion'
+import { effect } from '@preact/signals'
+import { InternalViewName } from '@schedule-x/shared/src/enums/calendar/internal-view.enum'
 
 class ScrollControllerPlugin implements PluginBase {
   name = PluginName.ScrollController
   private $app: CalendarAppSingleton | null = null
   private observer: MutationObserver | null = null
+  private hasScrolledSinceViewRender = false
 
   constructor(private config: ScrollControllerConfig = {}) {}
 
@@ -16,11 +19,34 @@ class ScrollControllerPlugin implements PluginBase {
    * */
   init($app: CalendarAppSingleton) {
     this.$app = $app
+    this.setInitialScroll($app)
+    this.setUpViewChangeEffect()
+  }
+
+  private setInitialScroll($app: CalendarAppSingleton) {
     const gridDay = (
       $app.elements.calendarWrapper as HTMLElement
     ).querySelector('.sx__time-grid-day')
+
     if (gridDay) this.scrollOnRender()
-    else this.observeIfGridDayExistsThenScroll()
+    else this.waitUntilGridDayExistsThenScroll()
+  }
+
+  private setUpViewChangeEffect() {
+    effect(() => {
+      this.hasScrolledSinceViewRender = false
+
+      if (
+        InternalViewName.Day === this.$app?.calendarState.view.value ||
+        InternalViewName.Week === this.$app?.calendarState.view.value
+      ) {
+        this.setInitialScroll(this.$app as CalendarAppSingleton)
+      }
+    })
+  }
+
+  private scrollOnRender() {
+    this.scrollTo(this.config.initialScroll || '07:50')
   }
 
   destroy(): void {
@@ -50,17 +76,20 @@ class ScrollControllerPlugin implements PluginBase {
     viewContainer.scroll(0, pixelsToScroll)
   }
 
-  private observeIfGridDayExistsThenScroll() {
-    /**
-     * Do not disconnect observer, because it should be used on every subsequent visit to the week- or day view
-     * */
+  private waitUntilGridDayExistsThenScroll() {
     this.observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         const gridDayExists = Array.from(mutation.addedNodes).find((node) =>
           (node as HTMLElement).classList.contains('sx__time-grid-day')
         )
-        if (mutation.type === 'childList' && gridDayExists) {
+        if (
+          mutation.type === 'childList' &&
+          gridDayExists &&
+          !this.hasScrolledSinceViewRender
+        ) {
           this.scrollOnRender()
+          this.hasScrolledSinceViewRender = true
+          this.observer?.disconnect()
         }
       })
     })
@@ -72,10 +101,6 @@ class ScrollControllerPlugin implements PluginBase {
         subtree: true,
       }
     )
-  }
-
-  private scrollOnRender() {
-    this.scrollTo(this.config.initialScroll || '07:50')
   }
 }
 
