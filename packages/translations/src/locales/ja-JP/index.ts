3f33eb904e86e066538c6c3395bde548176b29f2
Muramatsu <spring.43@icloud.com>
2024-01-16 21:01:01 +0900

    feat: add translations for japanese (#165)

---
+++ packages/translations/src/locales/ja-JP/index.ts
@@ -0,0 +1,8 @@
+import { datePickerJaJP } from './date-picker'
+import { Language } from '../../types/language.translations'
+import { calendarJaJP } from './calendar'
+
+export const jaJP: Language = {
+  ...datePickerJaJP,
+  ...calendarJaJP,
+}
