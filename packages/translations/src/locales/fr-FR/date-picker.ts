0dcbb79bc330371263bb73e9def64164b0c94710
Antoine LOZACH <antoine.lozach@cacd2.fr>
2024-02-05 16:35:02 +0100

    feat: add french lang

---
+++ packages/translations/src/locales/fr-FR/date-picker.ts
@@ -0,0 +1,10 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerFrFR: DatePickerTranslations = {
+  Date: 'Date',
+  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'Next month': 'Mois suivant',
+  'Previous month': 'Mois précédent',
+  'Choose Date': 'Choisir une date',
+  'Select View': 'Choisir la vue',
+}
---
+++ packages/translations/src/locales/fr-FR/index.ts
@@ -0,0 +1,8 @@
+import { datePickerFrFR } from './date-picker'
+import { Language } from '../../types/language.translations'
+import { calendarFrFR } from './calendar'
+
+export const frFR: Language = {
+  ...datePickerFrFR,
+  ...calendarFrFR,
+}
