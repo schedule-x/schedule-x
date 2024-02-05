d038570d7615d3e23610a6a82229878f4b32afc4
Antoine LOZACH <antoine.lozach@cacd2.fr>
2024-02-05 17:05:29 +0100

    fix: add proper locale date format from DD/MM/YYYY to JJ/MM/AAAA

--- packages/translations/src/locales/fr-FR/date-picker.ts
+++ packages/translations/src/locales/fr-FR/date-picker.ts
@@ -2,7 +2,7 @@ import { DatePickerTranslations } from '../../types/date-picker.translations'
 
 export const datePickerFrFR: DatePickerTranslations = {
   Date: 'Date',
-  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'MM/DD/YYYY': 'JJ/MM/AAAA',
   'Next month': 'Mois suivant',
   'Previous month': 'Mois précédent',
   'Choose Date': 'Choisir une date',
