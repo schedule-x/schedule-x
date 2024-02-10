8cb5a3a9b3d266d865c3fc44b5022b1bd5de7e32
Tom Österlund <tom.osterlund1@gmail.com>
2024-02-10 16:18:47 +0100

    test: fix recursion in test

--- packages/recurrence/src/recurrence-set/__test__/update-daily-rule.spec.ts
+++ packages/recurrence/src/recurrence-set/__test__/update-daily-rule.spec.ts
@@ -9,7 +9,7 @@ describe.only('Updating a recurrence set with a daily rule', () => {
   describe('Based on a date string', () => {
     it('should handle moving 3 days forward', () => {
       const initialValues = {
-        rrule: 'FREQ=DAILY;BYDAY=SA',
+        rrule: 'FREQ=DAILY;BYDAY=SA;UNTIL=20240220',
         dtstart: '20240210',
         dtend: '20240210',
       }
@@ -21,7 +21,7 @@ describe.only('Updating a recurrence set with a daily rule', () => {
       })
       rset.updateDtstart('20240213')
 
-      expect(rset.getRrule()).toEqual('FREQ=DAILY;BYDAY=TU')
+      expect(rset.getRrule()).toEqual('FREQ=DAILY;UNTIL=20240223;BYDAY=TU')
       expect(rset.getDtstart()).toEqual('20240213')
       expect(rset.getDtend()).toEqual('20240213')
     })
@@ -30,7 +30,7 @@ describe.only('Updating a recurrence set with a daily rule', () => {
   describe('Based on a datetime string', () => {
     it('should move 2 hours backwards', () => {
       const initialValues = {
-        rrule: 'FREQ=DAILY;BYDAY=SA',
+        rrule: 'FREQ=DAILY;BYDAY=SA;UNTIL=20240220T050000',
         dtstart: '20240210T040000',
         dtend: '20240210T050000',
       }
@@ -42,14 +42,16 @@ describe.only('Updating a recurrence set with a daily rule', () => {
       })
       rset.updateDtstart('20240210T020000')
 
-      expect(rset.getRrule()).toEqual('FREQ=DAILY;BYDAY=SA')
+      expect(rset.getRrule()).toEqual(
+        'FREQ=DAILY;UNTIL=20240220T030000;BYDAY=SA'
+      )
       expect(rset.getDtstart()).toEqual('20240210T020000')
       expect(rset.getDtend()).toEqual('20240210T030000')
     })
 
     it('should move 3 hours backwards into previous day', () => {
       const initialValues = {
-        rrule: 'FREQ=DAILY;BYDAY=SA',
+        rrule: 'FREQ=DAILY;BYDAY=SA;UNTIL=20240220T050000',
         dtstart: '20240210T003000',
         dtend: '20240210T013000',
       }
@@ -61,7 +63,9 @@ describe.only('Updating a recurrence set with a daily rule', () => {
       })
       rset.updateDtstart('20240209T213000')
 
-      expect(rset.getRrule()).toEqual('FREQ=DAILY;BYDAY=FR')
+      expect(rset.getRrule()).toEqual(
+        'FREQ=DAILY;UNTIL=20240220T020000;BYDAY=FR'
+      )
       expect(rset.getDtstart()).toEqual('20240209T213000')
       expect(rset.getDtend()).toEqual('20240209T223000')
     })
