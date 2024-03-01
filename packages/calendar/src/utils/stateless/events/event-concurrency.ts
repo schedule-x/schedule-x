d4c08b4b009c8c9a4921569c6e08b1ff13b21317
Tom Österlund <tom.osterlund1@gmail.com>
2024-03-01 18:03:46 +0100

    fix: overlap in concurrency with multiple events where only a few overlap

--- packages/calendar/src/utils/stateless/events/event-concurrency.ts
+++ packages/calendar/src/utils/stateless/events/event-concurrency.ts
@@ -5,38 +5,40 @@ export const handleEventConcurrency = (
   concurrentEventsCache: CalendarEventInternal[] = [],
   currentIndex = 0
 ): CalendarEventInternal[] => {
-  for (let i = currentIndex; i < sortedEvents.length; i++) {
-    const event = sortedEvents[i]
-    const nextEvent = sortedEvents[i + 1]
+  const i = currentIndex
+  const event = sortedEvents[i]
+  const nextEvent = sortedEvents[i + 1]
 
-    if (
-      concurrentEventsCache.length &&
-      (!nextEvent ||
-        concurrentEventsCache.every((e) => e.end < nextEvent.start))
-    ) {
-      concurrentEventsCache.push(event)
+  if (
+    concurrentEventsCache.length &&
+    (!nextEvent || concurrentEventsCache.every((e) => e.end < nextEvent.start))
+  ) {
+    concurrentEventsCache.push(event)
 
-      for (let ii = 0; ii < concurrentEventsCache.length; ii++) {
-        concurrentEventsCache[ii]._totalConcurrentEvents =
-          concurrentEventsCache.length
-        concurrentEventsCache[ii]._previousConcurrentEvents =
-          concurrentEventsCache.filter(
-            (e) =>
-              e.start < concurrentEventsCache[ii].start &&
-              e.end > concurrentEventsCache[ii].start
-          ).length
-      }
-      concurrentEventsCache = []
-      return handleEventConcurrency(sortedEvents, concurrentEventsCache, i + 1)
+    for (let ii = 0; ii < concurrentEventsCache.length; ii++) {
+      const currentEvent = concurrentEventsCache[ii]
+      currentEvent._totalConcurrentEvents = concurrentEventsCache.length
+      currentEvent._previousConcurrentEvents = concurrentEventsCache.filter(
+        (cachedEvent, index) => {
+          if (cachedEvent === currentEvent || index > ii) return false
+
+          return (
+            cachedEvent.start <= currentEvent.start &&
+            cachedEvent.end > currentEvent.start
+          )
+        }
+      ).length
     }
+    concurrentEventsCache = []
+    return handleEventConcurrency(sortedEvents, concurrentEventsCache, i + 1)
+  }
 
-    if (
-      (nextEvent && event.end > nextEvent.start) ||
-      concurrentEventsCache.some((e) => e.end > event.start)
-    ) {
-      concurrentEventsCache.push(event)
-      return handleEventConcurrency(sortedEvents, concurrentEventsCache, i + 1)
-    }
+  if (
+    (nextEvent && event.end > nextEvent.start) ||
+    concurrentEventsCache.some((e) => e.end > event.start)
+  ) {
+    concurrentEventsCache.push(event)
+    return handleEventConcurrency(sortedEvents, concurrentEventsCache, i + 1)
   }
 
   return sortedEvents
