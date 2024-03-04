6080e016cc74e5493b3377078220400598f7b246
Alvaro Cánepa <bfpdevel@gmail.com>
2024-03-04 04:25:32 -0300

    feat: add support for Spanish (#269)

---
+++ packages/translations/src/locales/es-ES/calendar.ts
@@ -0,0 +1,15 @@
+import { CalendarTranslations } from '../../types/calendar.translations'
+
+export const calendarEsES: CalendarTranslations = {
+  Today: 'Hoy',
+  Month: 'Mes',
+  Week: 'Semana',
+  Day: 'Día',
+  events: 'eventos',
+  event: 'evento',
+  'No events': 'Sin eventos',
+  'Next period': 'Siguiente período',
+  'Previous period': 'Período anterior',
+  to: 'a', // as in 2/1/2020 to 2/2/2020
+  'Full day- and multiple day events': 'Día completo y eventos de múltiples días',
+}
---
+++ packages/translations/src/locales/es-ES/index.ts
@@ -0,0 +1,8 @@
+import { datePickerEsES } from './date-picker'
+import { Language } from '../../types/language.translations'
+import { calendarEsES } from './calendar'
+
+export const esES: Language = {
+  ...datePickerEsES,
+  ...calendarEsES,
+}
