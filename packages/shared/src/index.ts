f011d98d48023bf3bc3d4f97713fa61cafec8fb2
Tom Österlund <tom.osterlund1@gmail.com>
2024-03-11 17:08:06 +0100

    feat: export CustomComponentFns API as CustomComponents

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ import CalendarAppSingleton from './interfaces/calendar/calendar-app-singleton'
 
 export type { View, ViewConfig } from './types/calendar/view'
 export type { CalendarAppSingleton }
+export type { CustomComponentFns as CustomComponents } from './interfaces/calendar/custom-component-fns'
 export {
   addDays,
   addMonths,
