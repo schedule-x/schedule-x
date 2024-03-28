9015bc90c923b7196ad609842760e243a61fa6bc
Dominik Baranec <dominik.baranec@scr.sk>
2024-03-28 08:55:22 +0100

    feat: add Slovak language support(#312)

---
+++ packages/translations/src/locales/sk-SK/calendar.ts
@@ -0,0 +1,15 @@
+import { CalendarTranslations } from '../../types/calendar.translations'
+
+export const calendarSkSK: CalendarTranslations = {
+  Today: 'Dnes',
+  Month: 'Mesiac',
+  Week: 'Týždeň',
+  Day: 'Deň',
+  events: 'udalosti',
+  event: 'udalosť',
+  'No events': 'Žiadne udalosti',
+  'Next period': 'Ďalšie obdobie',
+  'Previous period': 'Predchádzajúce obdobie',
+  to: 'do', // as in 2/1/2020 to 2/2/2020
+  'Full day- and multiple day events': 'Celodenné a viacdňové udalosti',
+}
