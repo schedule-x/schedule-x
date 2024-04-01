5ec04b96f40efba6430aa102aa91f91aec8ea0ac
Ilija Boshkov <ilija@codechem.com>
2024-04-01 09:38:59 +0200

    fix: fix DST recurrence calculation (#317)
    * fix: fix DST recurrence calculation
    
    * chore: remove log statement
    
    ---------
    
    Co-authored-by: Tom Österlund <tom.osterlund1@gmail.com>

--- packages/event-recurrence/src/__test__/create-recurrence.spec.ts
+++ packages/event-recurrence/src/__test__/create-recurrence.spec.ts
@@ -34,5 +34,32 @@ describe('Creating recurrences for events', () => {
       expect(events[3].start).toEqual('2024-02-25 16:00')
       expect(events[3].end).toEqual('2024-02-25 17:00')
     })
+
+    it('should create recurrence for all Saturdays in March 2024', () => {
+      const eventWithRRule: CalendarEventExternal = {
+        id: '1',
+        title: 'Weekly event',
+        start: '2024-03-02 16:00',
+        end: '2024-03-02 17:00',
+        rrule: 'FREQ=WEEKLY;BYDAY=SA;UNTIL=20240331T235959;',
+      }
+      const $app = __createAppWithViews__({
+        events: [eventWithRRule],
+      })
+      createEventRecurrencePlugin().init!($app)
+
+      const events = $app.calendarEvents.list.value
+      expect(events).toHaveLength(5)
+      expect(events[0].start).toEqual('2024-03-02 16:00')
+      expect(events[0].end).toEqual('2024-03-02 17:00')
+      expect(events[1].start).toEqual('2024-03-09 16:00')
+      expect(events[1].end).toEqual('2024-03-09 17:00')
+      expect(events[2].start).toEqual('2024-03-16 16:00')
+      expect(events[2].end).toEqual('2024-03-16 17:00')
+      expect(events[3].start).toEqual('2024-03-23 16:00')
+      expect(events[3].end).toEqual('2024-03-23 17:00')
+      expect(events[4].start).toEqual('2024-03-30 16:00')
+      expect(events[4].end).toEqual('2024-03-30 17:00')
+    })
   })
 })
