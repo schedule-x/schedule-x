02482a855ec9d9f1b1d1a97bfc0bec3ebef764a3
Ilija Boshkov <ilija@codechem.com>
2024-04-02 18:09:14 +0200

    feat: add macedonian language support (#321)

---
+++ packages/translations/src/locales/mk-MK/date-picker.ts
@@ -0,0 +1,10 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerMkMK: DatePickerTranslations = {
+  Date: 'Датум',
+  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'Next month': 'Следен месец',
+  'Previous month': 'Претходен месец',
+  'Choose Date': 'Избери Датум',
+  'Select View': 'Избери Преглед',
+}
