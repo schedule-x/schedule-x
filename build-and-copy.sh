5e41ecbf8b248c23c6cfeeafbc8735ad61c6d729
Tony DUONG <astowny@gmail.com>
2024-04-05 11:00:50 +0200

    feat(workflow): custom build to publish packages in a separated script

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -7,23 +7,3 @@ yarn build
 cp -r packages/ ../schedule-x-vue/
 
 rm -rf ../schedule-x-vue/packages/*/node_modules
-
-cd packages/calendar 
-
-npm publish --access public
-
-cd ../shared
-
-npm publish --access public
-
-cd ../sidebar
-
-npm publish --access public
-
-cd ../theme-default
-
-npm publish --access public
-
-cd ../translations
-
-npm publish --access public
---
+++ publish-my-packages.sh
@@ -0,0 +1,20 @@
+
+cd packages/calendar 
+
+npm publish --access public
+
+cd ../shared
+
+npm publish --access public
+
+cd ../sidebar
+
+npm publish --access public
+
+cd ../theme-default
+
+npm publish --access public
+
+cd ../translations
+
+npm publish --access public
