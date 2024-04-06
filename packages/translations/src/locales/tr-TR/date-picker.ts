7bbae58547c1279a4a3d5ec28826e5324efc2c5c
Muhammet KÖKLÜ <105980019+byhipernova@users.noreply.github.com>
2024-04-06 17:46:19 +0300

    feat: add turkish language support (#336)

---
+++ packages/translations/src/locales/tr-TR/date-picker.ts
@@ -0,0 +1,10 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerTrTR: DatePickerTranslations = {
+  Date: 'Tarih',
+  'MM/DD/YYYY': 'GG/AA/YYYY',
+  'Next month': 'Sonraki ay',
+  'Previous month': 'Önceki ay',
+  'Choose Date': 'Tarih Seç',
+  'Select View': 'Görünüm Seç',
+}
