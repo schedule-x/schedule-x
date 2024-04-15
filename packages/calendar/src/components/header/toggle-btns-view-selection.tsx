38b8dfb837fafa21adae8dc7a19f7d89aa31290b
Tony DUONG <astowny@gmail.com>
2024-04-15 18:05:48 +0200

    fix(calendar header): manage responsive btns

--- packages/calendar/src/components/header/toggle-btns-view-selection.tsx
+++ packages/calendar/src/components/header/toggle-btns-view-selection.tsx
@@ -1,16 +1,13 @@
-import { useContext, useState } from 'preact/hooks'
+import { useContext } from 'preact/hooks'
 import { AppContext } from '../../utils/stateful/app-context'
 import { InternalViewName } from '@schedule-x/shared/src/enums/calendar/internal-view.enum'
 
 export const ToggleBtnsViewSelection = () => {
   const $app = useContext(AppContext)
 
-  const [activeButton, setActiveButton] = useState('week')
-
   const handleChangeTimeFrame = (
     timeframe: 'day' | 'week' | 'month-grid' | 'month-agenda'
   ) => {
-    setActiveButton(timeframe)
     if (timeframe == 'day') {
       $app.calendarState.view.value = InternalViewName.Day
     } else if (timeframe == 'week') {
@@ -25,20 +22,26 @@ export const ToggleBtnsViewSelection = () => {
   return (
     <div className="time-frame-buttons">
       <button
-        className={`time-frame-button ${activeButton === 'day' ? 'active' : ''}`}
+        className={`time-frame-button ${$app.calendarState.view.value === 'day' ? 'active' : ''}`}
         onClick={() => handleChangeTimeFrame('day')}
       >
         {$app.translate('Day')}
       </button>
       <button
-        className={`time-frame-button ${activeButton === 'week' ? 'active' : ''}`}
+        className={`time-frame-button ${$app.calendarState.view.value === 'week' ? 'active' : ''}`}
         onClick={() => handleChangeTimeFrame('week')}
       >
         {$app.translate('Week')}
       </button>
       <button
-        className={`time-frame-button ${activeButton === 'month-grid' ? 'active' : ''}`}
-        onClick={() => handleChangeTimeFrame('month-grid')}
+        className={`time-frame-button ${[InternalViewName.MonthAgenda, InternalViewName.MonthGrid].includes($app.calendarState.view.value as InternalViewName) ? 'active' : ''}`}
+        onClick={() =>
+          handleChangeTimeFrame(
+            $app.calendarState.isCalendarSmall.value
+              ? 'month-agenda'
+              : 'month-grid'
+          )
+        }
       >
         {$app.translate('Month')}
       </button>
