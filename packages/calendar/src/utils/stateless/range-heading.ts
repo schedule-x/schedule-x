02811b87aa183d8ad1386ebf5056917beb8c31fe
Beksultan A <babdikaarov@gmail.com>
2024-04-15 23:50:33 +0300

    fix(date-time-localization.ts range-heading.ts format-conversion.ts): mM.toUpperCase ky-KG opt ru-RU
    if locale ru-RU => MM.WW => first letter to UpperCase. if ky-KG to opt ru-RU since Chromium has no
    current support for ky-KG.

--- packages/calendar/src/utils/stateless/range-heading.ts
+++ packages/calendar/src/utils/stateless/range-heading.ts
@@ -2,11 +2,51 @@ import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/cal
 import { toJSDate } from '@schedule-x/shared/src/utils/stateless/time/format-conversion/format-conversion'
 
 const getLocaleStringMonthArgs = ($app: CalendarAppSingleton) => {
+  if ($app.config.locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return ['ru-RU', { month: 'long' }] as const
+  }
   return [$app.config.locale, { month: 'long' }] as const
 }
 
 const getLocaleStringYearArgs = ($app: CalendarAppSingleton) => {
+  if ($app.config.locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return ['ru-RU', { year: 'numeric' }] as const
+  }
   return [$app.config.locale, { year: 'numeric' }] as const
+}
+
+// convertTo_RuRU_If_KyKG_or_RuRU needed since the browsers treat ru locale with lowerCase
+
+const convertToUpperCase_If_KyKG_or_RuRU_Locale = (
+  $app: CalendarAppSingleton,
+  rangeStart: string,
+  rangeEnd: string
+): string => {
+  let startDateMonth = toJSDate(rangeStart).toLocaleString(
+    ...getLocaleStringMonthArgs($app)
+  )
+
+  const startDateYear = toJSDate(rangeStart).toLocaleString(
+    ...getLocaleStringYearArgs($app)
+  )
+  let endDateMonth = toJSDate(rangeEnd).toLocaleString(
+    ...getLocaleStringMonthArgs($app)
+  )
+
+  const endDateYear = toJSDate(rangeEnd).toLocaleString(
+    ...getLocaleStringYearArgs($app)
+  )
+  startDateMonth = startDateMonth[0].toUpperCase() + startDateMonth.slice(1)
+  endDateMonth = endDateMonth[0].toUpperCase() + endDateMonth.slice(1)
+  if (startDateMonth === endDateMonth && startDateYear === endDateYear) {
+    return `${startDateMonth} ${startDateYear}`
+  } else if (startDateMonth !== endDateMonth && startDateYear === endDateYear) {
+    return `${startDateMonth} – ${endDateMonth} ${startDateYear}`
+  }
+
+  return `${startDateMonth} ${startDateYear} – ${endDateMonth} ${endDateYear}`
 }
 
 export const getMonthAndYearForDateRange = (
@@ -14,15 +54,22 @@ export const getMonthAndYearForDateRange = (
   rangeStart: string,
   rangeEnd: string
 ): string => {
+  if ($app.config.locale === 'ru-RU' || $app.config.locale === 'ky-KG') {
+    // convertTo_RuRU_If_KyKG_or_RuRU needed since the browsers treat ru locale with lowerCase
+    return convertToUpperCase_If_KyKG_or_RuRU_Locale($app, rangeStart, rangeEnd)
+  }
   const startDateMonth = toJSDate(rangeStart).toLocaleString(
     ...getLocaleStringMonthArgs($app)
   )
+
   const startDateYear = toJSDate(rangeStart).toLocaleString(
     ...getLocaleStringYearArgs($app)
   )
+
   const endDateMonth = toJSDate(rangeEnd).toLocaleString(
     ...getLocaleStringMonthArgs($app)
   )
+
   const endDateYear = toJSDate(rangeEnd).toLocaleString(
     ...getLocaleStringYearArgs($app)
   )
--- packages/shared/src/utils/stateless/time/date-time-localization/date-time-localization.ts
+++ packages/shared/src/utils/stateless/time/date-time-localization/date-time-localization.ts
@@ -1,8 +1,20 @@
 export const toLocalizedMonth = (date: Date, locale: string): string => {
+  if (locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return date.toLocaleString('ru-RU', { month: 'long' })
+  }
   return date.toLocaleString(locale, { month: 'long' })
 }
 
 export const toLocalizedDate = (date: Date, locale: string): string => {
+  if (locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return date.toLocaleString('ru-RU', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    })
+  }
   return date.toLocaleString(locale, {
     month: 'long',
     day: 'numeric',
@@ -11,6 +23,14 @@ export const toLocalizedDate = (date: Date, locale: string): string => {
 }
 
 export const toLocalizedDateString = (date: Date, locale: string): string => {
+  if (locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return date.toLocaleString('ru-RU', {
+      month: 'numeric',
+      day: 'numeric',
+      year: 'numeric',
+    })
+  }
   return date.toLocaleString(locale, {
     month: 'numeric',
     day: 'numeric',
@@ -23,12 +43,21 @@ export const getOneLetterDayNames = (
   locale: string
 ): string[] => {
   return week.map((date) => {
+    if (locale === 'ky-KG') {
+      // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+      return date.toLocaleString('ru-RU', { weekday: 'short' }).charAt(0)
+    }
     return date.toLocaleString(locale, { weekday: 'short' }).charAt(0)
   })
 }
 
-export const getDayNameShort = (date: Date, locale: string) =>
-  date.toLocaleString(locale, { weekday: 'short' })
+export const getDayNameShort = (date: Date, locale: string) => {
+  if (locale === 'ky-KG') {
+    // ky-KG has no support in chrome since has to opt for ru-RU lang as options as it's 99% readable in KG region
+    return date.toLocaleString('ru-RU', { weekday: 'short' })
+  }
+  return date.toLocaleString(locale, { weekday: 'short' })
+}
 
 export const getDayNamesShort = (week: Date[], locale: string): string[] => {
   return week.map((date) => getDayNameShort(date, locale))
