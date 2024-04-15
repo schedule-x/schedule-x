3862b105709d40ca4a6281be53495c3865442b74
Tom Österlund <tom.osterlund1@gmail.com>
2024-04-15 16:55:03 +0200

    fix: correct import statement

--- packages/event-recurrence/src/event-updater-plugin.impl.ts
+++ packages/event-recurrence/src/event-updater-plugin.impl.ts
@@ -1,5 +1,5 @@
 import PluginBase from '@schedule-x/shared/src/interfaces/plugin.interface'
-import { CalendarAppSingleton } from '@schedule-x/shared'
+import { CalendarAppSingleton } from '@schedule-x/shared/src'
 import EventsFacade from '@schedule-x/shared/src/utils/stateful/events-facade/events-facade.interface'
 import { EventsFacadeImpl } from './util/stateful/events-facade'
 import CalendarEventExternal from '@schedule-x/shared/src/interfaces/calendar/calendar-event.interface'
