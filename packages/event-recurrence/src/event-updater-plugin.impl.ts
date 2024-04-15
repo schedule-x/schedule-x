1cb7b350576d61ed4a6caabd080e186393af2f41
Tom Österlund <tom.osterlund1@gmail.com>
2024-04-15 08:29:18 +0200

    feat: add event-updater plugin for event-recurrence package

---
+++ packages/event-recurrence/src/event-updater-plugin.impl.ts
@@ -0,0 +1,43 @@
+import PluginBase from '@schedule-x/shared/src/interfaces/plugin.interface'
+import { CalendarAppSingleton } from '@schedule-x/shared'
+import EventsFacade from '@schedule-x/shared/src/utils/stateful/events-facade/events-facade.interface'
+import { EventsFacadeImpl } from './util/stateful/events-facade'
+import CalendarEventExternal from '@schedule-x/shared/src/interfaces/calendar/calendar-event.interface'
+
+class EventUpdaterPluginImpl implements PluginBase {
+  name: string = 'EventUpdaterPlugin'
+  $app!: CalendarAppSingleton
+  eventsFacade!: EventsFacade
+
+  init($app: CalendarAppSingleton): void {
+    this.$app = $app
+    this.eventsFacade = new EventsFacadeImpl(this.$app)
+  }
+
+  add(event: CalendarEventExternal): void {
+    this.eventsFacade.add(event)
+  }
+
+  update(event: CalendarEventExternal): void {
+    this.eventsFacade.update(event)
+  }
+
+  remove(eventId: string): void {
+    this.eventsFacade.remove(eventId)
+  }
+
+  get(eventId: string): CalendarEventExternal | undefined {
+    return this.eventsFacade.get(eventId)
+  }
+
+  getAll(): CalendarEventExternal[] {
+    return this.eventsFacade.getAll()
+  }
+
+  set(events: CalendarEventExternal[]): void {
+    this.eventsFacade.set(events)
+  }
+}
+
+export const createEventUpdaterPlugin = (): EventUpdaterPluginImpl =>
+  new EventUpdaterPluginImpl()
