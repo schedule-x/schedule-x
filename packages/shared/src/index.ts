e3b287d18c95b696d5a892f439613780f9e383a0
Tom Österlund <tom.osterlund1@gmail.com>
2024-04-15 17:11:03 +0200

    feat: expose interfaces for external config and events

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,8 @@
 import CalendarAppSingleton from './interfaces/calendar/calendar-app-singleton'
+import type CalendarEvent from './interfaces/calendar/calendar-event.interface'
 
+export type { CalendarConfigExternal } from './interfaces/calendar/calendar-config'
+export type { CalendarEvent }
 export type { View, ViewConfig } from './types/calendar/view'
 export type { CalendarAppSingleton }
 export type { CustomComponentFns as CustomComponents } from './interfaces/calendar/custom-component-fns'
