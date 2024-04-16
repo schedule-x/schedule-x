d91c20eb731346bb3682b088233d5eabe90d173e
Beksultan A <babdikaarov@gmail.com>
2024-04-16 17:53:34 +0300

    refactor(seqance fix remmove comment): remove comment

--- packages/translations/src/locales/kg-KG/months-weekdays.ts
+++ packages/translations/src/locales/kg-KG/months-weekdays.ts
@@ -2,10 +2,9 @@ import { MonthsWeekdaysTranslations } from '../../types/months-weeks.translation
 
 export const monthsWeekdaysKgKG: MonthsWeekdaysTranslations = {
   monthsName:
-    // 'Январь_Февраль_Март_Апрель_Май_Июнь_Июль_Август_Сентябрь_Октябрь_Ноябрь_Декабрь'.split(
     'Үчтүн айы_Бирдин айы_Жалган Куран_Чын Куран_Бугу_Кулжа_Теке_Баш Оона_Аяк Оона_Тогуздун айы_Жетинин айы_Бештин айы'.split(
       '_'
     ),
   weekdaysName:
-    'Дүйшөмбү_Шейшемби_Шаршемби_Бейшемби_Жума_Ишемби_Жекшемби'.split('_'),
+    'Жекшемби_Дүйшөмбү_Шейшемби_Шаршемби_Бейшемби_Жума_Ишемби'.split('_'),
 }
