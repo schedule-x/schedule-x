b9becbd361f993f984268b4091e7267f1dea3b2d
Beksultan A <babdikaarov@gmail.com>
2024-04-16 21:33:01 +0300

    ci(eslintrc.cjs): allow translatinns to be injected in shared

--- eslintrc.cjs
+++ eslintrc.cjs
@@ -63,7 +63,7 @@ module.exports = {
           },
           {
             from: 'shared',
-            allow: [],
+            allow: ['translations'],
           },
           {
             from: 'translations',
