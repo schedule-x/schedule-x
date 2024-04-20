3378869a46e6f157eb45be1de9b74f3172f57fc0
Tom Österlund <tom.osterlund1@gmail.com>
2024-04-20 11:32:08 +0200

    feat(calendar): expose interfaces

--- packages/calendar/src/index.ts
+++ packages/calendar/src/index.ts
@@ -7,6 +7,7 @@ import {
   CalendarConfigExternal as CalendarConfig,
   CustomComponentFn,
 } from '@schedule-x/shared/src/interfaces/calendar/calendar-config'
+import { CalendarEvent } from '@schedule-x/shared/src'
 import CalendarApp from './calendar.app'
 import { createPreactView } from './utils/stateful/preact-view/preact-view'
 import {
@@ -15,7 +16,7 @@ import {
   setRangeForDay,
 } from './utils/stateless/time/range/set-range'
 
-export type { CalendarConfig, CustomComponentFn }
+export type { CalendarConfig, CustomComponentFn, CalendarEvent }
 
 export {
   createCalendar,
