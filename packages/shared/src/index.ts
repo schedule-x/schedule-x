5e9b6e771ee832aa6f74b9345eef395201eba1ea
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2024-04-30 06:29:05 +0200

    feat(shared): expose more interfaces (#394)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,8 +1,10 @@
 import CalendarAppSingleton from './interfaces/calendar/calendar-app-singleton'
 import type CalendarEvent from './interfaces/calendar/calendar-event.interface'
+import type { CalendarEventInternal } from './interfaces/calendar/calendar-event.interface'
+import type EventModalPlugin from './interfaces/event-modal/event-modal.plugin'
 
 export type { CalendarConfigExternal } from './interfaces/calendar/calendar-config'
-export type { CalendarEvent }
+export type { CalendarEvent, EventModalPlugin, CalendarEventInternal }
 export type { View, ViewConfig } from './types/calendar/view'
 export type { CalendarAppSingleton }
 export type { CustomComponentFns as CustomComponents } from './interfaces/calendar/custom-component-fns'
