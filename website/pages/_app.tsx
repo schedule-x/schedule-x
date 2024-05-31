6e1b295a848edc7b21a0dda4eabf2a5f31051cea
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2024-05-31 15:12:30 +0200

    docs: theme switch on landing page (#446)

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { Podkova } from 'next/font/google'
 import '../theme/index.scss'
 import '../components/pages/landing-page.scss'
 import '../components/pages/calendar-demo.scss'
 import '../components/partials/card/card.scss'
 
+const podkova = Podkova({
+  subsets: ['latin-ext'],
+})
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={podkova.className + ' main'}><Component {...pageProps} /></main>
 }
