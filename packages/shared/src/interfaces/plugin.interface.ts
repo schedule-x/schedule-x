76fa96c0d56da4715dcfc75386753be50c7f80e1
Tom Österlund <tom.osterlund1@gmail.com>
2024-06-20 05:52:25 +0200

    fix(calendar): use recursive partial for $app interface in plugins

--- packages/shared/src/interfaces/plugin.interface.ts
+++ packages/shared/src/interfaces/plugin.interface.ts
@@ -1,11 +1,20 @@
 import CalendarAppSingleton from './calendar/calendar-app-singleton'
+import { RecursivePartial } from '../types/recursive-partial'
 
 export default interface PluginBase {
   name: string
 
   // TODO v2: change to `beforeRender`
-  beforeInit?($app: CalendarAppSingleton): void
+  /**
+   * use a recursive partial on the $app interface, in order to not break any third party plugins
+   * when we add new properties to the $app interface
+   * */
+  beforeInit?($app: RecursivePartial<CalendarAppSingleton>): void
   // TODO v2: change to `onRender` and remove $app parameter
-  init?($app: CalendarAppSingleton): void
+  /**
+   * use a recursive partial on the $app interface, in order to not break any third party plugins
+   * when we add new properties to the $app interface
+   * */
+  init?($app: RecursivePartial<CalendarAppSingleton>): void
   destroy?(): void
 }
