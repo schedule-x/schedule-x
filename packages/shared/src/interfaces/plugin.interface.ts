66446581d96d6345dc1d84e9f9ad79b1da306ad5
Tom Österlund <tom.osterlund1@gmail.com>
2024-06-22 21:12:22 +0200

    fix(calendar): make typing of $app flexible for plugins

--- packages/shared/src/interfaces/plugin.interface.ts
+++ packages/shared/src/interfaces/plugin.interface.ts
@@ -1,20 +1,29 @@
 import CalendarAppSingleton from './calendar/calendar-app-singleton'
-import { RecursivePartial } from '../types/recursive-partial'
 
 export default interface PluginBase {
   name: string
 
   // TODO v2: change to `beforeRender`
   /**
-   * use a recursive partial on the $app interface, in order to not break any third party plugins
-   * when we add new properties to the $app interface
+   * Allow implementers to dynamically add any properties to the global app object as they see fit.
+   * In order to avoid conflict with future properties added to the library, we recommend
+   * using the unique prefix `$` for any custom properties added to the global app object.
+   * for example $app['$websocketService'] = new WebsocketService().
+   * Adding properties to existing sub-objects is discouraged, since this will make your application more
+   * brittle to future changes in the library.
    * */
-  beforeInit?($app: RecursivePartial<CalendarAppSingleton>): void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  beforeInit?($app: CalendarAppSingleton | any): void
   // TODO v2: change to `onRender` and remove $app parameter
   /**
-   * use a recursive partial on the $app interface, in order to not break any third party plugins
-   * when we add new properties to the $app interface
+   * Allow implementers to dynamically add any properties to the global app object as they see fit.
+   * In order to avoid conflict with future properties added to the library, we recommend
+   * using the unique prefix `$` for any custom properties added to the global app object.
+   * for example $app['$websocketService'] = new WebsocketService().
+   * Adding properties to existing sub-objects is discouraged, since this will make your application more
+   * brittle to future changes in the library.
    * */
-  init?($app: RecursivePartial<CalendarAppSingleton>): void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  init?($app: CalendarAppSingleton | any): void
   destroy?(): void
 }
