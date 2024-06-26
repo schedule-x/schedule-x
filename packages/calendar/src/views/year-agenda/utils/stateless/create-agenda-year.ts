1137b90ac92bb95e7041c712bea5c74e49cbc902
Dove <33008055+dovepa@users.noreply.github.com>
2024-06-26 08:38:37 +0200

    fix: fix error on createAgendaYear

--- packages/calendar/src/views/year-agenda/utils/stateless/create-agenda-year.ts
+++ packages/calendar/src/views/year-agenda/utils/stateless/create-agenda-year.ts
@@ -1,10 +1,9 @@
 import { toDateString } from '@schedule-x/shared/src'
-import { Month } from '@schedule-x/shared/src/enums/time/month.enum'
 import { YearAgenda as YearAgendaType } from '../../types/year-agenda'
 import { toIntegers } from '@schedule-x/shared/src/utils/stateless/time/format-conversion/format-conversion'
 export const createAgendaYear = (date: string): YearAgendaType => {
   const { year } = toIntegers(date)
-  return Object.entries(Month).map((month, index) => {
+  return new Array(12).fill({}).map((data, index) => {
     const date = new Date()
     date.setFullYear(year, index, 1)
     return {
