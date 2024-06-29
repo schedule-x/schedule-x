7f32caebcabbd448d7556e06b25c5c90e1ddd3b4
Tom Österlund <tom.osterlund1@gmail.com>
2024-06-29 11:24:34 +0200

    fix(date-picker): add new image for date picker

--- development/date-picker/main.tsx
+++ development/date-picker/main.tsx
@@ -14,7 +14,7 @@ const datePicker = createDatePicker({
   // locale: 'fr-FR',
   style: {
     fullWidth: true,
-    dark: true,
+    // dark: true,
   },
   // locale: 'sv-SE',
   firstDayOfWeek: 0,
