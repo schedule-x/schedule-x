b53a6260cc9a4a58a281d7e71bc18c236035f540
Jonah Matteson <main@insberr.com>
2024-07-11 21:01:45 -0700

    feat: export functions for converting between Date object and string Export `toDateString`, `toTimeString`, and `toJSDate`

--- packages/calendar/src/index.ts
+++ packages/calendar/src/index.ts
@@ -7,7 +7,12 @@ import {
   CalendarConfigExternal as CalendarConfig,
   CustomComponentFn,
 } from '@schedule-x/shared/src/interfaces/calendar/calendar-config'
-import { CalendarEvent } from '@schedule-x/shared/src'
+import {
+  CalendarEvent,
+  toDateString,
+  toTimeString,
+  toJSDate,
+} from '@schedule-x/shared/src'
 import CalendarApp from './calendar.app'
 import { createPreactView } from './utils/stateful/preact-view/preact-view'
 import {
@@ -16,7 +21,6 @@ import {
   setRangeForDay,
 } from './utils/stateless/time/range/set-range'
 import { externalEventToInternal } from '@schedule-x/shared/src/utils/stateless/calendar/external-event-to-internal'
-
 export type { CalendarConfig, CustomComponentFn, CalendarEvent }
 
 export {
@@ -26,6 +30,9 @@ export {
   viewDay,
   viewMonthAgenda,
   CalendarApp,
+  toDateString,
+  toTimeString,
+  toJSDate,
   createPreactView,
   setRangeForDay,
   setRangeForWeek,
