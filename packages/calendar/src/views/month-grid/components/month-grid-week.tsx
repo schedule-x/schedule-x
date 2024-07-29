0885f9543708d999f221b0c71a1bffbd6bc86bf3
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2024-07-29 12:51:52 +0200

    fix: throw away state of month grid days when navigating through months (#570)

--- packages/calendar/src/views/month-grid/components/month-grid-week.tsx
+++ packages/calendar/src/views/month-grid/components/month-grid-week.tsx
@@ -14,13 +14,20 @@ export default function MonthGridWeek({
 }: props) {
   return (
     <div className="sx__month-grid-week">
-      {week.map((day) => (
-        <MonthGridDay
-          day={day}
-          isFirstWeek={isFirstWeek}
-          isLastWeek={isLastWeek}
-        />
-      ))}
+      {week.map((day) => {
+        /**
+         * The day component keeps internal state, and needs to be thrown away once the day changes.
+         * */
+        const dateKey = day.date
+        return (
+          <MonthGridDay
+            key={dateKey}
+            day={day}
+            isFirstWeek={isFirstWeek}
+            isLastWeek={isLastWeek}
+          />
+        )
+      })}
     </div>
   )
 }
