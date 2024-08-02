d3522ee5b25012eb393bc85389d13f406444608c
Tom Österlund <tom.osterlund1@gmail.com>
2024-08-02 08:40:37 +0200

    fix: build problem

--- packages/date-picker/src/index.ts
+++ packages/date-picker/src/index.ts
@@ -1,5 +1,8 @@
 import { createDatePicker, createDatePickerInternal } from './factory'
 import { DatePickerConfigExternal } from '@schedule-x/shared/src/interfaces/date-picker/config.interface'
 
-export { createDatePicker, createDatePickerInternal, DatePickerConfigExternal }
+export { createDatePicker, createDatePickerInternal }
 export type DatePickerConfig = DatePickerConfigExternal
+
+// For the Vue component
+export interface IDatePickerConfig extends DatePickerConfig {}
