c2ad0aafaccfd40a4ff88dd9d56c1767c1bac959
Anderson <acontreras@admira.com>
2024-08-05 12:26:39 +0200

    Added Catala translation

---
+++ packages/translations/src/locales/ca-ES/date-picker.ts
@@ -0,0 +1,10 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerCaES: DatePickerTranslations = {
+  Date: 'Data',
+  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'Next month': 'Següent mes',
+  'Previous month': 'Mes anterior8',
+  'Choose Date': 'Selecciona una data',
+  'Select View': 'Selecciona una vista',
+}
---
+++ packages/translations/src/locales/ca-ES/index.ts
@@ -0,0 +1,8 @@
+import { datePickerCaES } from './date-picker'
+import { Language } from '../../types/language.translations'
+import { calendarCaES } from './calendar'
+
+export const caES: Language = {
+  ...datePickerCaES,
+  ...calendarCaES,
+}
