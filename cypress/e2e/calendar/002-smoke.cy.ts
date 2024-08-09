8829d53f4a6333a3e206ace5b70d20b9b4554d5a
Tom Österlund <tom.osterlund1@gmail.com>
2024-08-09 07:26:39 +0200

    test: wait before screenshot

--- cypress/e2e/calendar/002-smoke.cy.ts
+++ cypress/e2e/calendar/002-smoke.cy.ts
@@ -17,14 +17,14 @@ describe('Calendar Smoke Test', () => {
 
   it('should render events in the month view', () => {
     calendarHeader.openViewByLabel('Month')
-    cy.wait(100)
+    cy.wait(300)
     cy.compareSnapshot('smoke-test__month-view', SNAPSHOT_FAULT_TOLERANCE)
   })
 
   it('should render events in the day view', () => {
     cy.wait(1000) // for scrolling to finish
     calendarHeader.openViewByLabel('Day')
-    cy.wait(100)
+    cy.wait(300)
     cy.compareSnapshot('smoke-test__day-view', SNAPSHOT_FAULT_TOLERANCE)
   })
 })
