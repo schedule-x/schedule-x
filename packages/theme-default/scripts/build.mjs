447499476ed861cd97479c4aec67278eb950d03e
Tom Österlund <tom.osterlund1@gmail.com>
2024-08-26 18:52:44 +0200

    fix(theme-default): remove source maps for scss

--- packages/theme-default/scripts/build.mjs
+++ packages/theme-default/scripts/build.mjs
@@ -2,10 +2,10 @@ import { execSync } from 'child_process'
 
 function build() {
   try {
-    execSync('sass ./src/index.scss ./dist/index.css')
-    execSync('sass ./src/date-picker.scss ./dist/date-picker.css')
-    execSync('sass ./src/time-picker.scss ./dist/time-picker.css')
-    execSync('sass ./src/calendar.scss ./dist/calendar.css')
+    execSync('sass --no-source-map ./src/index.scss ./dist/index.css')
+    execSync('sass --no-source-map ./src/date-picker.scss ./dist/date-picker.css')
+    execSync('sass --no-source-map ./src/time-picker.scss ./dist/time-picker.css')
+    execSync('sass --no-source-map ./src/calendar.scss ./dist/calendar.css')
   } catch (e) {
     console.error(e)
   }
