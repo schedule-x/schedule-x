53f8ea6d9de5612aad55c41a0956af50e6bf3a55
Tom Österlund <tom.osterlund1@gmail.com>
2024-09-16 21:32:30 +0200

    feat: wip

--- packages/icalendar/src/__test__/icalendar-plugin-impl.spec.ts
+++ packages/icalendar/src/__test__/icalendar-plugin-impl.spec.ts
@@ -8,9 +8,34 @@ import { createIcalendarPlugin } from '../icalendar-plugin.impl'
 describe('IcalendarPluginImpl', () => {
   describe('parse plugins', () => {
     it('should parse icalendar source', () => {
-      const plugin = createIcalendarPlugin()
+      const plugin = createIcalendarPlugin({
+        source: 'BEGIN:VCALENDAR\n' +
+          'VERSION:2.0\n' +
+          'CALSCALE:GREGORIAN\n' +
+          'BEGIN:VEVENT\n' +
+          'SUMMARY:Good morning\n' +
+          'DTSTART;TZID=America/New_York:20130802T103400\n' +
+          'DTEND;TZID=America/New_York:20130802T110400\n' +
+          'LOCATION:1000 Broadway Ave.\\, Brooklyn\n' +
+          'DESCRIPTION: Access-A-Ride trip to 900 Jay St.\\, Brooklyn\n' +
+          'STATUS:CONFIRMED\n' +
+          'SEQUENCE:3\n' +
+          'END:VEVENT\n' +
+          'BEGIN:VEVENT\n' +
+          'RRULE:FREQ=DAILY;COUNT=3\n' +
+          'SUMMARY:Good night\n' +
+          'DTSTART;TZID=America/New_York:20130802T200000\n' +
+          'DTEND;TZID=America/New_York:20130802T203000\n' +
+          'LOCATION:900 Jay St.\\, Brooklyn\n' +
+          'DESCRIPTION: Access-A-Ride trip to 1000 Broadway Ave.\\, Brooklyn\n' +
+          'STATUS:CONFIRMED\n' +
+          'SEQUENCE:3\n' +
+          'END:VEVENT\n' +
+          'END:VCALENDAR'
+      })
       const $app = __createAppWithViews__({
         plugins: [plugin],
+        selectedDate: '2013-08-02',
       })
       plugin.beforeInit($app)
     })
--- packages/icalendar/src/icalendar-plugin.impl.ts
+++ packages/icalendar/src/icalendar-plugin.impl.ts
@@ -1,98 +1,71 @@
-import { CalendarAppSingleton, PluginBase } from '@schedule-x/shared/src'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { CalendarAppSingleton, CalendarEvent, PluginBase, toDateTimeString, toJSDate } from '@schedule-x/shared/src'
 import ICAL from 'ical.js'
-import { CalendarEvent } from '@schedule-x/shared/src'
-import { randomStringId, toDateTimeString } from '@schedule-x/shared/src'
+import { IcalExpander } from './ical-expander/IcalExpander'
 
-// type ICALDate = { toJSDate: () => Date }
+type ICalendarPluginOptions = {
+  source: string
+}
 
 class IcalendarPluginImpl implements PluginBase {
-  name = 'IcalendarPlugin'
+  name = 'ICalendarPlugin'
   private $app!: CalendarAppSingleton
+  private source: string = ''
 
   private events: CalendarEvent[] = []
 
+  constructor(private options: ICalendarPluginOptions) {
+    this.source = options.source
+  }
+
   beforeInit($app: CalendarAppSingleton) {
     this.$app = $app
-    this.parseIcalendarSource(
-      'BEGIN:VCALENDAR\n' +
-        'VERSION:2.0\n' +
-        'CALSCALE:GREGORIAN\n' +
-        'BEGIN:VEVENT\n' +
-        'SUMMARY:Access-A-Ride Pickup\n' +
-        'DTSTART;TZID=America/New_York:20130802T103400\n' +
-        'DTEND;TZID=America/New_York:20130802T110400\n' +
-        'LOCATION:1000 Broadway Ave.\\, Brooklyn\n' +
-        'DESCRIPTION: Access-A-Ride trip to 900 Jay St.\\, Brooklyn\n' +
-        'STATUS:CONFIRMED\n' +
-        'SEQUENCE:3\n' +
-        'BEGIN:VALARM\n' +
-        'TRIGGER:-PT10M\n' +
-        'DESCRIPTION:Pickup Reminder\n' +
-        'ACTION:DISPLAY\n' +
-        'END:VALARM\n' +
-        'END:VEVENT\n' +
-        'BEGIN:VEVENT\n' +
-        'RRULE:FREQ=DAILY;COUNT=3\n' +
-        'SUMMARY:Access-A-Ride Pickup\n' +
-        'DTSTART;TZID=America/New_York:20130802T200000\n' +
-        'DTEND;TZID=America/New_York:20130802T203000\n' +
-        'LOCATION:900 Jay St.\\, Brooklyn\n' +
-        'DESCRIPTION: Access-A-Ride trip to 1000 Broadway Ave.\\, Brooklyn\n' +
-        'STATUS:CONFIRMED\n' +
-        'SEQUENCE:3\n' +
-        'BEGIN:VALARM\n' +
-        'TRIGGER:-PT10M\n' +
-        'DESCRIPTION:Pickup Reminder\n' +
-        'ACTION:DISPLAY\n' +
-        'END:VALARM\n' +
-        'END:VEVENT\n' +
-        'END:VCALENDAR'
-    )
+    let dateRangeStart = $app.calendarState.range.value?.start
+    let dateRangeEnd = $app.calendarState.range.value?.end
+    if (!dateRangeStart || !dateRangeEnd) return
+
+    const after = toJSDate(dateRangeStart)
+    const before = toJSDate(dateRangeEnd)
+    this.parseIcalendarSource(after, before)
   }
 
-  private parseIcalendarSource(source: string) {
-    const jcalData = ICAL.parse(source)
-    const component = new ICAL.Component(jcalData)
-    const events = component
-      .getAllSubcomponents('vevent')
-      .map((vevent) => new ICAL.Event(vevent))
-      .forEach(this.expandEvent)
+  private parseIcalendarSource(after: Date, before: Date) {
+    const icalExpander = new IcalExpander({
+      ics: this.source,
+    })
+
+    const { occurrences, events } = icalExpander.between(after, before)
+
+    this.events = [
+      ...occurrences.map(this.icalOccurrenceToSXEvent),
+      ...events.map(this.icalEventToSXEvent),
+    ]
+    console.log(this.events)
   }
 
-  private expandEvent = (eventComp: ICAL.Event) => {
-    const sxEvent: CalendarEvent = {
-      start: toDateTimeString(eventComp.startDate.toJSDate()),
-      end: toDateTimeString(eventComp.endDate.toJSDate()),
-      title: eventComp.summary,
-      description: eventComp.description,
-      location: eventComp.location,
-      id: randomStringId(),
-    }
 
-    this.events.push(sxEvent)
-
-    if (eventComp.isRecurring()) {
-      this.expandOccurrences(eventComp)
+  private icalOccurrenceToSXEvent(occurrence: ICAL.Event): CalendarEvent {
+    return {
+      id: occurrence.eventId,
+      title: occurrence.item.summary,
+      description: occurrence.item.description,
+      location: occurrence.item.location,
+      start: toDateTimeString(occurrence.startDate.toJSDate()),
+      end: toDateTimeString(occurrence.endDate.toJSDate()),
     }
   }
 
-  private expandOccurrences(event: ICAL.Event) {
-    const iterator = event.iterator()
-    let occurrence: ICAL.Event | null
-    while ((occurrence = iterator.next())) {
-      const occurrence: CalendarEvent = {
-        start: toDateTimeString(occurrence.startDate.toJSDate()),
-        end: toDateTimeString(occurrence.endDate.toJSDate()),
-        title: occurrence.summary,
-        description: occurrence.description,
-        location: occurrence.location,
-        id: randomStringId(),
-      }
-      this.events.push(occurrence)
+  private icalEventToSXEvent(event: ICAL.Event): CalendarEvent {
+    return {
+      id: event.eventId,
+      title: event.summary,
+      description: event.description,
+      location: event.location,
+      start: toDateTimeString(event.startDate.toJSDate()),
+      end: toDateTimeString(event.endDate.toJSDate()),
     }
   }
 }
 
-export const createIcalendarPlugin = () => new IcalendarPluginImpl()
+export const createIcalendarPlugin = (options: ICalendarPluginOptions) => {
+  return new IcalendarPluginImpl(options)
+}
