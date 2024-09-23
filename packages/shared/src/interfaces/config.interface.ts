1ad4bc057f45ab329aab2934085f08f5842e4896
Yehuda Ringler <yehuda.ringler@chabad.org>
2024-09-23 14:30:39 -0400

    feat: config timeunits

--- packages/shared/src/interfaces/config.interface.ts
+++ packages/shared/src/interfaces/config.interface.ts
@@ -1,5 +1,6 @@
 import { WeekDay } from '../enums/time/week-day.enum'
 import { Signal } from '@preact/signals'
+import TimeUnits from '../utils/stateful/time-units/time-units.interface'
 
 /**
  * This interface serves as a bridge between the config interface for the date picker amd the calendar.
@@ -7,4 +8,5 @@ import { Signal } from '@preact/signals'
 export default interface Config {
   locale: Signal<string>
   firstDayOfWeek: Signal<WeekDay>
+  timeUnits: TimeUnits
 }
