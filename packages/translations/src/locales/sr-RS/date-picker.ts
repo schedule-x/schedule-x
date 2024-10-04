e941ccea52d9b529e5bf83f4857cf83ce0bdb468
alexva22 <123355258+alexva22@users.noreply.github.com>
2024-10-04 17:18:51 +0200

    feat: add serbian translations (#714)
    * Add serbian language.
    
    * Update supported-languages.mdx
    
    ---------
    
    Co-authored-by: Tom Österlund <63011648+tomosterlund@users.noreply.github.com>

---
+++ packages/translations/src/locales/sr-RS/date-picker.ts
@@ -0,0 +1,10 @@
+import { DatePickerTranslations } from '../../types/date-picker.translations'
+
+export const datePickerSrRs: DatePickerTranslations = {
+  Date: 'Datum',
+  'MM/DD/YYYY': 'DD/MM/YYYY',
+  'Next month': 'Sledeći mesec',
+  'Previous month': 'Prethodni mesec',
+  'Choose Date': 'Izaberite datum',
+  'Select View': 'Odaberite pregled',
+}
