1cc4978c7e6bb8c0727d610efeaefa01c92a78d0
Justin Barca <4888331+justin-hackin@users.noreply.github.com>
2024-10-08 12:14:08 -0400

    test: add example

--- development/calendar/main.tsx
+++ development/calendar/main.tsx
@@ -5,28 +5,19 @@ import '@fontsource/open-sans/500-italic.css'
 import '@fontsource/open-sans/700.css'
 import '@fontsource/open-sans/700-italic.css'
 import '@fontsource/roboto-condensed'
-import {
-  createCalendar,
-} from '@schedule-x/calendar/src'
+import {createCalendar,} from '@schedule-x/calendar/src'
 import '../../packages/theme-default/src/calendar.scss'
 import '../app.css'
-import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop/src'
-import { createEventModalPlugin } from '@schedule-x/event-modal/src'
-import { seededEvents } from '../data/seeded-events.ts'
-import { createScrollControllerPlugin } from '@schedule-x/scroll-controller/src'
-import { createResizePlugin } from '../../packages/resize/src'
-import {
-  createEventRecurrencePlugin,
-  createEventsServicePlugin,
-} from '@schedule-x/event-recurrence/src'
-import { createCalendarControlsPlugin } from '../../packages/calendar-controls/src'
-import { CalendarAppSingleton } from '@schedule-x/shared/src'
-import { createCurrentTimePlugin } from '../../packages/current-time/src/current-time-plugin.impl.ts'
-import { createViewMonthGrid } from '@schedule-x/calendar/src/views/month-grid'
-import { createViewWeek } from '@schedule-x/calendar/src/views/week'
-import { createViewDay } from '@schedule-x/calendar/src/views/day'
-import { createViewMonthAgenda } from '@schedule-x/calendar/src/views/month-agenda'
+import {createScrollControllerPlugin} from '@schedule-x/scroll-controller/src'
+import {createEventsServicePlugin,} from '@schedule-x/event-recurrence/src'
+import {createCalendarControlsPlugin} from '../../packages/calendar-controls/src'
+import {CalendarAppSingleton} from '@schedule-x/shared/src'
+import {createViewMonthGrid} from '@schedule-x/calendar/src/views/month-grid'
+import {createViewWeek} from '@schedule-x/calendar/src/views/week'
+import {createViewDay} from '@schedule-x/calendar/src/views/day'
+import {createViewMonthAgenda} from '@schedule-x/calendar/src/views/month-agenda'
 import {WeekDay} from "@schedule-x/shared/src/enums/time/week-day.enum.ts";
+import {createIcalendarPlugin} from "@schedule-x/ical/src";
 
 const calendarElement = document.getElementById('calendar') as HTMLElement
 
@@ -67,7 +58,38 @@ const calendarsUpdaterPlugin = new CalendarsUpdaterPlugin()
 
 const calendarControlsPlugin = createCalendarControlsPlugin()
 const eventsServicePlugin = createEventsServicePlugin()
-
+const icalendarPlugin = createIcalendarPlugin({
+  data:
+    "BEGIN:VCALENDAR\n" +
+    "PRODID:-//Google Inc//Google Calendar 70.9054//EN\n" +
+    "VERSION:2.0\n" +
+    "CALSCALE:GREGORIAN\n" +
+    "METHOD:PUBLISH\n" +
+    "X-WR-CALNAME:Test ICS\n" +
+    "X-WR-TIMEZONE:America/Toronto\n" +
+    "BEGIN:VEVENT\n" +
+    "DTSTART;VALUE=DATE:20241025\n" +
+    "DTEND;VALUE=DATE:20241026\n" +
+    "DTSTAMP:20241007T123831Z\n" +
+    "ORGANIZER;CN=4c1a3b795346d897c8781a0df30cde8a2fc054d2fed1daa16e2ddfe5ec8b59\n" +
+    " 0f@group.calendar.google.com:mailto:4c1a3b795346d897c8781a0df30cde8a2fc054d\n" +
+    " 2fed1daa16e2ddfe5ec8b590f@group.calendar.google.com\n" +
+    "UID:2k29n7a4iimvdaoi54jv42s5mu@google.com\n" +
+    "CREATED:20241007T123804Z\n" +
+    'DESCRIPTION:More Info: <a href="https://www.google.com/url?q=https://www.in\n' +
+    " stagram.com/p/C9cl6SGA04s/&amp;sa=D&amp;source=calendar&amp;usd=2&amp;u\n" +
+    ' sg=AOvVaw042wfimCgCk3ioqphPZ0jF" target="_blank">https://www.instagram.com/\n' +
+    " p/C9cl6SGA04s/</a>\n" +
+    "LAST-MODIFIED:20241007T123804Z\n" +
+    "LOCATION:EVO Kitchen & Bar, 31 Water St S, Cambridge, ON N1R 3C7, Canad\n" +
+    " a\n" +
+    "SEQUENCE:0\n" +
+    "STATUS:CONFIRMED\n" +
+    "SUMMARY:Feel Soul GOOD\n" +
+    "TRANSP:TRANSPARENT\n" +
+    "END:VEVENT\n" +
+    "END:VCALENDAR\n"
+});
 const calendar = createCalendar({
   weekOptions: {
     // gridHeight: 3000,
@@ -110,7 +132,7 @@ const calendar = createCalendar({
     },
 
     onRangeUpdate(range) {
-      console.log('onRangeUpdate', range)
+      icalendarPlugin.between(range.start, range.end)
     },
 
     onEventUpdate(event) {
@@ -153,152 +175,83 @@ const calendar = createCalendar({
     //   return $app.elements.calendarWrapper!.clientWidth! < 500
     // }
   },
-  calendars: {
-    personal: {
-      colorName: 'personal',
-      lightColors: {
-        main: '#f9d71c',
-        container: '#fff5aa',
-        onContainer: '#594800',
-      },
-      darkColors: {
-        main: '#fff5c0',
-        onContainer: '#fff5de',
-        container: '#a29742',
-      },
-    },
-    work: {
-      colorName: 'work',
-      lightColors: {
-        main: '#f91c45',
-        container: '#ffd2dc',
-        onContainer: '#59000d',
-      },
-      darkColors: {
-        main: '#ffc0cc',
-        onContainer: '#ffdee6',
-        container: '#a24258',
-      },
-    },
-    leisure: {
-      colorName: 'leisure',
-      lightColors: {
-        main: '#1cf9b0',
-        container: '#dafff0',
-        onContainer: '#004d3d',
-      },
-      darkColors: {
-        main: '#c0fff5',
-        onContainer: '#e6fff5',
-        container: '#42a297',
-      },
-    },
-    school: {
-      colorName: 'school',
-      lightColors: {
-        main: '#1c7df9',
-        container: '#d2e7ff',
-        onContainer: '#002859',
-      },
-      darkColors: {
-        main: '#c0dfff',
-        onContainer: '#dee6ff',
-        container: '#426aa2',
-      },
-    },
-  },
-  events: [
-    {
-      id: 874574875,
-      start: '2024-09-09 07:45',
-      end: '2024-09-09 09:01',
-      _customContent: {
-        timeGrid: '<div class="custom-content">Custom Content</div>',
-        monthGrid: '<div class="custom-content">Custom Content</div>',
-      }
-    },
-    {
-      id: 874574875,
-      start: '2024-09-09',
-      end: '2024-09-09',
-      title: 'All Day Event',
-      _customContent: {
-        dateGrid: '<div class="custom-content">Custom Content</div>',
-        monthAgenda: '<div class="custom-content">Custom Content</div>',
-      }
-    },
-    ...seededEvents,
-    {
-      id: 45678,
-      title: 'Bi-Weekly Event Monday and Wednesday',
-      start: '2024-03-19 14:00',
-      end: '2024-03-19 15:00',
-      rrule: 'FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959',
-    },
-    {
-      id: 18547854,
-      title: 'Bi-Weekly Event Monday and Wednesday',
-      start: '2024-02-05 14:00',
-      end: '2024-02-05 15:00',
-      rrule: 'FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959',
-    },
-    {
-      id: 18547855,
-      title: 'Weekly Event',
-      start: '2024-02-03',
-      end: '2024-02-03',
-      rrule: 'FREQ=WEEKLY;COUNT=4',
-    },
-    {
-      id: 789,
-      title: 'Daily event',
-      start: '2024-02-05 12:00',
-      end: '2024-02-05 13:55',
-      rrule: 'FREQ=DAILY;COUNT=5',
-      calendarId: 'personal',
-    },
-    {
-      id: 9834876578,
-      title: 'Daily event 2',
-      start: '2024-02-05 12:00',
-      end: '2024-02-05 13:55',
-      rrule: 'FREQ=DAILY;UNTIL=20240209T235900',
-      calendarId: 'work',
-    },
-    {
-      id: 7845684678465874,
-      title: 'Monthly event',
-      start: '2024-02-07 16:00',
-      end: '2024-02-07 17:55',
-      rrule: 'FREQ=MONTHLY;COUNT=5',
-    },
-    {
-      rrule: 'FREQ=YEARLY;COUNT=5',
-      title: 'Yearly event',
-      start: '2024-02-08 16:00',
-      end: '2024-02-08 17:55',
-      id: 874367853,
-    },
-  ],
+  // events: [
+  //   {
+  //     id: 874574875,
+  //     start: '2024-09-09 07:45',
+  //     end: '2024-09-09 09:01',
+  //     _customContent: {
+  //       timeGrid: '<div class="custom-content">Custom Content</div>',
+  //       monthGrid: '<div class="custom-content">Custom Content</div>',
+  //     }
+  //   },
+  //   {
+  //     id: 874574875,
+  //     start: '2024-09-09',
+  //     end: '2024-09-09',
+  //     title: 'All Day Event',
+  //     _customContent: {
+  //       dateGrid: '<div class="custom-content">Custom Content</div>',
+  //       monthAgenda: '<div class="custom-content">Custom Content</div>',
+  //     }
+  //   },
+  //   ...seededEvents,
+  //   {
+  //     id: 45678,
+  //     title: 'Bi-Weekly Event Monday and Wednesday',
+  //     start: '2024-03-19 14:00',
+  //     end: '2024-03-19 15:00',
+  //     rrule: 'FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959',
+  //   },
+  //   {
+  //     id: 18547854,
+  //     title: 'Bi-Weekly Event Monday and Wednesday',
+  //     start: '2024-02-05 14:00',
+  //     end: '2024-02-05 15:00',
+  //     rrule: 'FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959',
+  //   },
+  //   {
+  //     id: 18547855,
+  //     title: 'Weekly Event',
+  //     start: '2024-02-03',
+  //     end: '2024-02-03',
+  //     rrule: 'FREQ=WEEKLY;COUNT=4',
+  //   },
+  //   {
+  //     id: 789,
+  //     title: 'Daily event',
+  //     start: '2024-02-05 12:00',
+  //     end: '2024-02-05 13:55',
+  //     rrule: 'FREQ=DAILY;COUNT=5',
+  //     calendarId: 'personal',
+  //   },
+  //   {
+  //     id: 9834876578,
+  //     title: 'Daily event 2',
+  //     start: '2024-02-05 12:00',
+  //     end: '2024-02-05 13:55',
+  //     rrule: 'FREQ=DAILY;UNTIL=20240209T235900',
+  //     calendarId: 'work',
+  //   },
+  //   {
+  //     id: 7845684678465874,
+  //     title: 'Monthly event',
+  //     start: '2024-02-07 16:00',
+  //     end: '2024-02-07 17:55',
+  //     rrule: 'FREQ=MONTHLY;COUNT=5',
+  //   },
+  //   {
+  //     rrule: 'FREQ=YEARLY;COUNT=5',
+  //     title: 'Yearly event',
+  //     start: '2024-02-08 16:00',
+  //     end: '2024-02-08 17:55',
+  //     id: 874367853,
+  //   },
+  // ],
 }, [
-  createDragAndDropPlugin(),
-  createCalendarControlsPlugin(),
-  createScrollControllerPlugin(),
-  createEventsServicePlugin(),
-  createCurrentTimePlugin(),
-  createEventModalPlugin(),
-  createEventRecurrencePlugin(),
-  createResizePlugin(),
+  icalendarPlugin
 ])
 calendar.render(calendarElement)
-
-console.log(calendar.scrollController.scrollTo)
-console.log(calendar.dragAndDrop.getTimePointsForIntervalConfig)
-console.log(calendar.calendarControls)
-console.log(calendar.eventsService.getAll())
-console.log(calendar.resize.minutesPerInterval)
-console.log(calendar.eventRecurrence.updateRecurrenceOnResize)
-console.log(calendar.eventModal.close)
 
 // const calendar2Element = document.getElementById('calendar-2') as HTMLElement
 // const calendar2 = createCalendar({
