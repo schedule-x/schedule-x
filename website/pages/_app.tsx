711d6f95efaf2dc0172afbf3636aec4b80533b4c
Tom Österlund <tom.osterlund1@gmail.com>
2024-11-15 07:05:32 +0100

    docs: fix font loading

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -14,19 +14,16 @@ import '@sx-premium/interactive-event-modal/index.css'
 import '@sx-premium/drag-to-create/index.css'
 import '@sx-premium/resource-scheduler/index.css'
 import 'rsuite/dist/rsuite-no-reset.min.css';
+import '@fontsource-variable/podkova';
 
 import Script from 'next/script'
-
-const podkova = Podkova({
-  subsets: ['latin-ext'],
-})
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   let alxScript = false
   if (typeof window === 'object') alxScript = true
 
-  return <main className={podkova.className + ' main'}><Component {...pageProps} />
+  return <main className={'main'}><Component {...pageProps} />
     {alxScript && window?.location.hostname !== 'localhost' && <Script data-domain="schedule-x.dev" src="https://analytics.schedule-x.com/js/script.js" defer />}
     <Script>
       {`window.lemonSqueezyAffiliateConfig = { store: "schedule-x" };`}
