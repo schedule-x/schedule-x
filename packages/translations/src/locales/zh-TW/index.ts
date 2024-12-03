b4ca1cdf06c84679d768af7dd831231661171a12
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2024-12-03 03:41:00 +0000

    update: zhTW

--- packages/translations/src/locales/zh-TW/index.ts
+++ packages/translations/src/locales/zh-TW/index.ts
@@ -2,7 +2,7 @@ import { datePickerZhTW } from './date-picker'
 import { Language } from '../../types/language.translations'
 import { calendarZhTW } from './calendar'
 
-export const zhCN: Language = {
+export const zhTW: Language = {
   ...datePickerZhTW,
   ...calendarZhTW,
 }
