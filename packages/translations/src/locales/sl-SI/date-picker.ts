3f167b12a554e693689e31416ed28265e9328729
Mark Debeljak <mark.debeljak@gmail.com>
2025-01-19 22:55:12 +0100

    Added Slovenian (sl-SI) translations

---
+++ packages/translations/src/locales/sl-SI/date-picker.ts
@@ -0,0 +1,9 @@
+import { DatePickerTranslations } from '@schedule-x/shared/src/types/translations/date-picker.translations'
+
+export const datePickerSlSI: DatePickerTranslations = {
+  Date: 'Datum',
+  'MM/DD/YYYY': 'MM.DD.YYYY',
+  'Next month': 'Naslednji mesec',
+  'Previous month': 'Prejšnji mesec',
+  'Choose Date': 'Izberi datum',
+}
