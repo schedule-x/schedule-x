94a5d112e8cbbacce1db2fbe31cf22924ddd7719
Patrik Galina <patrik@domore.hr>
2025-01-20 23:39:13 +0100

    chore: add hr translations

---
+++ packages/translations/src/locales/hr-HR/index.ts
@@ -0,0 +1,8 @@
+import { datePickerHrHR } from './date-picker'
+import { Language } from '@schedule-x/shared/src/types/translations/language.translations'
+import { calendarHrHR } from './calendar'
+
+export const hrHr: Language = {
+  ...datePickerHrHR,
+  ...calendarHrHR,
+}
