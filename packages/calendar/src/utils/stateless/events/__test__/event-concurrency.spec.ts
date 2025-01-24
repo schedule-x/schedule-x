4486fbef1ba7bd97626ad5de622f2eb4e675f2d5
Billy Street-Rose <bstreetrose@lincoln.ac.uk>
2025-01-24 14:23:27 +0000

    chore: more unit tests

--- packages/calendar/src/utils/stateless/events/__test__/event-concurrency.spec.ts
+++ packages/calendar/src/utils/stateless/events/__test__/event-concurrency.spec.ts
@@ -41,39 +41,13 @@ describe('Event concurrency', () => {
         end: '2020-01-01 03:00',
         id: '4',
       }
-      const timeEvent5 = {
-        start: '2025-01-28 13:30',
-        end: '2025-01-28 17:00',
-        id: '5',
-      }
-      const timeEvent6 = {
-        start: '2025-01-28 14:00',
-        end: '2025-01-28 15:00',
-        id: '6',
-      }
-      const timeEvent7 = {
-        start: '2025-01-28 15:00',
-        end: '2025-01-28 16:30',
-        id: '7',
-      }
 
       const event1 = createEvent(timeEvent1)
       const event2 = createEvent(timeEvent2)
       const event3 = createEvent(timeEvent3)
       const event4 = createEvent(timeEvent4)
-      const event5 = createEvent(timeEvent5)
-      const event6 = createEvent(timeEvent6)
-      const event7 = createEvent(timeEvent7)
 
-      const result = handleEventConcurrency([
-        event1,
-        event2,
-        event3,
-        event4,
-        event5,
-        event6,
-        event7,
-      ])
+      const result = handleEventConcurrency([event1, event2, event3, event4])
 
       expect(result[0]._totalConcurrentEvents).toBe(3)
       expect(result[0]._previousConcurrentEvents).toBe(0)
@@ -87,15 +61,6 @@ describe('Event concurrency', () => {
       expect(result[3]._totalConcurrentEvents).toBe(1)
       expect(result[3]._previousConcurrentEvents).toBe(0)
       expect(result[3]._maxConcurrentEvents).toBe(1)
-      expect(result[4]._totalConcurrentEvents).toBe(3)
-      expect(result[4]._previousConcurrentEvents).toBe(0)
-      expect(result[4]._maxConcurrentEvents).toBe(2)
-      expect(result[5]._totalConcurrentEvents).toBe(2)
-      expect(result[5]._previousConcurrentEvents).toBe(1)
-      expect(result[5]._maxConcurrentEvents).toBe(2)
-      expect(result[6]._totalConcurrentEvents).toBe(2)
-      expect(result[6]._previousConcurrentEvents).toBe(1)
-      expect(result[6]._maxConcurrentEvents).toBe(2)
     })
 
     it('should not be concurrent, if event1 ends at the same time event2 starts', () => {
@@ -114,8 +79,10 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(1)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(1)
       expect(result[1]._totalConcurrentEvents).toBe(1)
       expect(result[1]._previousConcurrentEvents).toBe(0)
+      expect(result[1]._maxConcurrentEvents).toBe(1)
     })
 
     it('should be concurrent, if event1 overlaps event2 by 1 minute', () => {
@@ -134,8 +101,10 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(2)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(2)
       expect(result[1]._totalConcurrentEvents).toBe(2)
       expect(result[1]._previousConcurrentEvents).toBe(1)
+      expect(result[1]._maxConcurrentEvents).toBe(2)
     })
 
     it('should have 5 concurrent events, but 2-5 should only have 1 or 2 previous concurrent event', () => {
@@ -175,14 +144,19 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(5)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(3)
       expect(result[1]._totalConcurrentEvents).toBe(3)
       expect(result[1]._previousConcurrentEvents).toBe(1)
+      expect(result[1]._maxConcurrentEvents).toBe(3)
       expect(result[2]._totalConcurrentEvents).toBe(3)
       expect(result[2]._previousConcurrentEvents).toBe(2)
+      expect(result[2]._maxConcurrentEvents).toBe(3)
       expect(result[3]._totalConcurrentEvents).toBe(2)
       expect(result[3]._previousConcurrentEvents).toBe(1)
+      expect(result[3]._maxConcurrentEvents).toBe(2)
       expect(result[4]._totalConcurrentEvents).toBe(2)
       expect(result[4]._previousConcurrentEvents).toBe(1)
+      expect(result[4]._maxConcurrentEvents).toBe(2)
     })
 
     it('should have 3 concurrent events where 1 and 2 start at the same time', () => {
@@ -206,10 +180,13 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(3)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(3)
       expect(result[1]._totalConcurrentEvents).toBe(3)
       expect(result[1]._previousConcurrentEvents).toBe(1)
+      expect(result[1]._maxConcurrentEvents).toBe(3)
       expect(result[2]._totalConcurrentEvents).toBe(3)
       expect(result[2]._previousConcurrentEvents).toBe(2)
+      expect(result[2]._maxConcurrentEvents).toBe(3)
     })
 
     it('should have 5 concurrent events where 1, 2, 3 start at the same time, 4 and 5 do not overlap', () => {
@@ -249,14 +226,19 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(5)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(3)
       expect(result[1]._totalConcurrentEvents).toBe(3)
       expect(result[1]._previousConcurrentEvents).toBe(1)
+      expect(result[1]._maxConcurrentEvents).toBe(3)
       expect(result[2]._totalConcurrentEvents).toBe(3)
       expect(result[2]._previousConcurrentEvents).toBe(2)
+      expect(result[2]._maxConcurrentEvents).toBe(3)
       expect(result[3]._totalConcurrentEvents).toBe(2)
       expect(result[3]._previousConcurrentEvents).toBe(1)
+      expect(result[3]._maxConcurrentEvents).toBe(2)
       expect(result[4]._totalConcurrentEvents).toBe(2)
       expect(result[4]._previousConcurrentEvents).toBe(1)
+      expect(result[4]._maxConcurrentEvents).toBe(2)
     })
 
     it('should have two concurrent events, where 2 starts after 1, and also ends after 1', () => {
@@ -275,8 +257,43 @@ describe('Event concurrency', () => {
 
       expect(result[0]._totalConcurrentEvents).toBe(2)
       expect(result[0]._previousConcurrentEvents).toBe(0)
+      expect(result[0]._maxConcurrentEvents).toBe(2)
       expect(result[1]._totalConcurrentEvents).toBe(2)
       expect(result[1]._previousConcurrentEvents).toBe(1)
+      expect(result[1]._maxConcurrentEvents).toBe(2)
     })
+  })
+
+  it('should have 1 event with 2 other consecutive events happening alongside', () => {
+    const timeEvent1 = {
+      start: '2025-01-28 13:30',
+      end: '2025-01-28 17:00',
+      id: '5',
+    }
+    const timeEvent2 = {
+      start: '2025-01-28 14:00',
+      end: '2025-01-28 15:00',
+      id: '6',
+    }
+    const timeEvent3 = {
+      start: '2025-01-28 15:00',
+      end: '2025-01-28 16:30',
+      id: '7',
+    }
+    const event1 = createEvent(timeEvent1)
+    const event2 = createEvent(timeEvent2)
+    const event3 = createEvent(timeEvent3)
+
+    const result = handleEventConcurrency([event1, event2, event3])
+
+    expect(result[0]._totalConcurrentEvents).toBe(3)
+    expect(result[0]._previousConcurrentEvents).toBe(0)
+    expect(result[0]._maxConcurrentEvents).toBe(2)
+    expect(result[1]._totalConcurrentEvents).toBe(2)
+    expect(result[1]._previousConcurrentEvents).toBe(1)
+    expect(result[1]._maxConcurrentEvents).toBe(2)
+    expect(result[2]._totalConcurrentEvents).toBe(2)
+    expect(result[2]._previousConcurrentEvents).toBe(1)
+    expect(result[2]._maxConcurrentEvents).toBe(2)
   })
 })
