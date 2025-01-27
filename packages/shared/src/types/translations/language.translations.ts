15d004108f4b44d4e36a2051f5a650f5c6c35a71
Tom Österlund <tom.osterlund1@gmail.com>
2025-01-27 17:56:51 +0100

    feat(translations): add merge locales function

--- packages/shared/src/types/translations/language.translations.ts
+++ packages/shared/src/types/translations/language.translations.ts
@@ -1,4 +1,6 @@
 import { DatePickerTranslations } from './date-picker.translations'
 import { CalendarTranslations } from './calendar.translations'
 
-export type Language = DatePickerTranslations & CalendarTranslations
+export type Language = DatePickerTranslations &
+  CalendarTranslations &
+  Record<string, string> // enable custom & premium plugins to use the default translator
