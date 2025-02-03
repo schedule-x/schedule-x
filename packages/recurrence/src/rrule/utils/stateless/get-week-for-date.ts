a237414eca95f2c8000620fa05fcac6032b4fe08
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2025-02-03 09:01:51 +0100

    feat(event-recurrence): enable using wkst in rrule (#911)

--- packages/recurrence/src/rrule/utils/stateless/get-week-for-date.ts
+++ packages/recurrence/src/rrule/utils/stateless/get-week-for-date.ts
@@ -1,11 +1,30 @@
 import { toJSDate } from '@schedule-x/shared/src/utils/stateless/time/format-conversion/format-conversion'
 import { toDateString } from '@schedule-x/shared/src/utils/stateless/time/format-conversion/date-to-strings'
 
-export const getWeekForDate = (date: string) => {
+function getFirstDateOfWeek(
+  date: Date,
+  firstDayOfWeek: 0 | 1 | 2 | 3 | 4 | 5 | 6
+) {
+  const dateIsNthDayOfWeek = date.getDay() - firstDayOfWeek
+
+  const firstDateOfWeek = date
+  if (dateIsNthDayOfWeek === 0) {
+    return firstDateOfWeek
+  } else if (dateIsNthDayOfWeek > 0) {
+    firstDateOfWeek.setDate(date.getDate() - dateIsNthDayOfWeek)
+  } else {
+    firstDateOfWeek.setDate(date.getDate() - (7 + dateIsNthDayOfWeek))
+  }
+
+  return firstDateOfWeek
+}
+
+export const getWeekForDate = (
+  date: string,
+  firstDayOfWeek: 0 | 1 | 2 | 3 | 4 | 5 | 6 = 0
+) => {
   const dateJS = toJSDate(date)
-  const dayOfWeek = dateJS.getDay()
-  const startOfWeek = new Date(dateJS)
-  startOfWeek.setDate(dateJS.getDate() - dayOfWeek)
+  const startOfWeek = getFirstDateOfWeek(dateJS, firstDayOfWeek)
   startOfWeek.setHours(0, 0, 0, 0)
 
   return Array.from({ length: 7 }).map((_, index) => {
