89b59544f76aeb5180726c49b45b1bf063fb1309
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2025-02-05 09:11:18 +0100

    feat: extend custom component fns type (#915)
    * feat: extend custom component fns type
    
    - make it possible to add a custom component with any string as name
    - expose custom components name type

--- packages/shared/src/interfaces/calendar/custom-component-fns.ts
+++ packages/shared/src/interfaces/calendar/custom-component-fns.ts
@@ -1,15 +1,19 @@
 import { CustomComponentFn } from './calendar-config'
 
+export type CustomComponentName =
+  | 'timeGridEvent'
+  | 'dateGridEvent'
+  | 'monthGridEvent'
+  | 'monthAgendaEvent'
+  | 'eventModal'
+  | 'headerContentLeftPrepend'
+  | 'headerContentLeftAppend'
+  | 'headerContentRightPrepend'
+  | 'headerContentRightAppend'
+  | 'headerContent'
+  | 'interactiveModalAdditionalFields'
+  | string
+
 export type CustomComponentFns = {
-  timeGridEvent?: CustomComponentFn
-  dateGridEvent?: CustomComponentFn
-  monthGridEvent?: CustomComponentFn
-  monthAgendaEvent?: CustomComponentFn
-  eventModal?: CustomComponentFn
-  headerContentLeftPrepend?: CustomComponentFn
-  headerContentLeftAppend?: CustomComponentFn
-  headerContentRightPrepend?: CustomComponentFn
-  headerContentRightAppend?: CustomComponentFn
-  headerContent?: CustomComponentFn
-  interactiveModalAdditionalFields?: CustomComponentFn
+  [key in CustomComponentName]?: CustomComponentFn
 }
