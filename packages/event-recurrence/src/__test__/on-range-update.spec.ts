b5cb95ecbcbca73acab84f9148ccf911877fb572
Tom Österlund <tom.osterlund1@gmail.com>
2025-02-12 08:40:27 +0100

    test(event-recurrence): enable rrule for background events

--- packages/event-recurrence/src/__test__/on-range-update.spec.ts
+++ packages/event-recurrence/src/__test__/on-range-update.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import {
   describe,
   it,
@@ -6,6 +7,7 @@ import {
 import CalendarEventExternal from '@schedule-x/shared/src/interfaces/calendar/calendar-event.interface'
 import { __createAppWithViews__ } from '@schedule-x/calendar/src/utils/stateless/testing/__create-app-with-views__'
 import { createEventRecurrencePlugin } from '../event-recurrence-plugin.impl'
+import { BackgroundEvent } from '@schedule-x/shared/src/interfaces/calendar/background-event'
 
 describe('limiting infinite events to the current calendar range', () => {
   describe('when rendering the calendar in month view', () => {
@@ -98,6 +100,63 @@ describe('limiting infinite events to the current calendar range', () => {
 
       events = $app.calendarEvents.list.value
       expect(events).toHaveLength(6)
+    })
+  })
+})
+
+describe('limiting infinite background events to the current calendar range', () => {
+  describe('when rendering the calendar in month view', () => {
+    it('should limit an infinite background event with weekly frequency', () => {
+      const backgroundEventWithRRule: BackgroundEvent = {
+        start: '2025-02-12',
+        end: '2025-02-12',
+        rrule: 'FREQ=WEEKLY;BYDAY=WE,FR;INTERVAL=2;',
+        style: {
+          backgroundColor: 'red',
+        },
+      }
+      const $app = __createAppWithViews__({
+        backgroundEvents: [backgroundEventWithRRule],
+        selectedDate: '2025-02-01',
+        defaultView: 'month-grid',
+      })
+
+      const eventRecurrencePlugin = createEventRecurrencePlugin()
+      eventRecurrencePlugin.beforeRender!($app)
+
+      const backgroundEvents = $app.calendarEvents.backgroundEvents.value
+      expect(backgroundEvents).toHaveLength(4)
+      const firstEvent = backgroundEvents[0]
+      expect(firstEvent.start).toBe('2025-02-12')
+      expect(firstEvent.end).toBe('2025-02-12')
+      const secondEvent = backgroundEvents[1]
+      expect(secondEvent.start).toBe('2025-02-14')
+      expect(secondEvent.end).toBe('2025-02-14')
+      const thirdEvent = backgroundEvents[2]
+      expect(thirdEvent.start).toBe('2025-02-26')
+      expect(thirdEvent.end).toBe('2025-02-26')
+      const fourthEvent = backgroundEvents[3]
+      expect(fourthEvent.start).toBe('2025-02-28')
+      expect(fourthEvent.end).toBe('2025-02-28')
+
+      eventRecurrencePlugin.onRangeUpdate!({
+        start: '2025-03-01 00:00',
+        end: '2025-03-31 23:59',
+      })
+
+      const updatedBackgroundEvents = $app.calendarEvents.backgroundEvents.value
+      expect(updatedBackgroundEvents).toHaveLength(8)
+      const fifthEvent = updatedBackgroundEvents[4]
+      expect(fifthEvent.start).toBe('2025-03-12')
+      expect(fifthEvent.end).toBe('2025-03-12')
+      const sixthEvent = updatedBackgroundEvents[5]
+      expect(sixthEvent.start).toBe('2025-03-14')
+      expect(sixthEvent.end).toBe('2025-03-14')
+      const seventhEvent = updatedBackgroundEvents[6]
+      expect(seventhEvent.start).toBe('2025-03-26')
+      expect(seventhEvent.end).toBe('2025-03-26')
+      const eighthEvent = updatedBackgroundEvents[7]
+      expect(eighthEvent.start).toBe('2025-03-28')
     })
   })
 })
