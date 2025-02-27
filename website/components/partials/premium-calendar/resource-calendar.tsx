b946492eaf7ce283fa0647c69033f3e73b84f1c8
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2025-02-27 07:44:17 +0100

    docs: resource grouping (#936)

--- website/components/partials/premium-calendar/resource-calendar.tsx
+++ website/components/partials/premium-calendar/resource-calendar.tsx
@@ -43,6 +43,28 @@ export default function ResourceCalendar() {
     {
       id: 'conveyor-belt-a',
       label: 'Conveyor Belt A',
+      resources: [
+        {
+          id: 'conveyor-belt-a-1',
+          label: 'Conveyor Belt A 1',
+          colorName: 'belt-a-1',
+          lightColors: {
+            main: '#1cf9b0',
+            container: '#dafff0',
+            onContainer: '#004d3d',
+          },
+        },
+        {
+          id: 'conveyor-belt-a-2',
+          label: 'Conveyor Belt A 2',
+          colorName: 'belt-a-2',
+          lightColors: {
+            main: '#1c7df9',
+            container: '#d2e7ff',
+            onContainer: '#002859',
+          }
+        }
+      ],
     },
     {
       id: 'conveyor-belt-b',
@@ -111,92 +133,92 @@ export default function ResourceCalendar() {
     selectedDate: '2024-05-06',
     events: [
       {
-        id: 1,
+        id: '1',
         title: 'Tom',
         start: '2024-05-06 02:00',
         end: '2024-05-06 07:55',
         resourceId: 'conveyor-belt-b',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 2,
+        id: '2',
         title: 'Deepak',
         start: '2024-05-06 02:00',
         end: '2024-05-06 07:55',
         resourceId: 'conveyor-belt-b',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 3,
+        id: '3',
         title: 'Tim',
         start: '2024-05-06 00:00',
         end: '2024-05-06 03:00',
         resourceId: 'welding',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 4,
+        id: '4',
         title: 'Zhao',
         start: '2024-05-06 01:30',
         end: '2024-05-06 03:30',
         resourceId: 'cnc',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 5,
+        id: '5',
         title: 'Ted',
         start: '2024-05-06 00:00',
         end: '2024-05-06 08:00',
         resourceId: 'supervision-1',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 6,
+        id: '6',
         title: 'Chen',
         start: '2024-05-06 06:00',
         end: '2024-05-06 09:00',
         resourceId: 'welding',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 7,
+        id: '7',
         title: 'Jane',
         start: '2024-05-06 07:30',
         end: '2024-05-06 09:30',
         resourceId: 'cnc',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 8,
+        id: '8',
         title: 'Marsha',
         start: '2024-05-06 08:00',
         end: '2024-05-06 14:00',
-        resourceId: 'conveyor-belt-a',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        resourceId: 'conveyor-belt-a-1',
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 0,
+        id: '0',
         title: 'Jane',
         start: '2024-05-06 08:00',
         end: '2024-05-06 14:00',
-        resourceId: 'conveyor-belt-a',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        resourceId: 'conveyor-belt-a-2',
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 9,
+        id: '9',
         title: 'Huang',
         start: '2024-05-06 08:00',
         end: '2024-05-06 13:55',
         resourceId: 'conveyor-belt-b',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
       {
-        id: 10,
+        id: '10',
         title: 'Freddy',
         start: '2024-05-06 08:00',
         end: '2024-05-06 13:55',
         resourceId: 'conveyor-belt-b',
-        rrule: 'FREQ=DAILY;UNTIL=20240510T235959'
+        rrule: 'FREQ=DAILY;COUNT=5'
       },
     ],
     calendars: calendars,
