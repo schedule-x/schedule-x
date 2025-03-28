fc3c7b0bec9d895eb12038ac16df4bba57225ec2
Tom Österlund <tom.osterlund1@gmail.com>
2025-03-28 07:52:18 +0100

    feat: add rtl support

---
+++ packages/translations/src/locales/he-IL/date-picker.ts
@@ -0,0 +1,9 @@
+import { DatePickerTranslations } from '@schedule-x/shared/src/types/translations/date-picker.translations'
+
+export const datePickerHeIL: DatePickerTranslations = {
+  Date: 'תַאֲרִיך',
+  'MM/DD/YYYY': 'MM/DD/YYYY',
+  'Next month': 'חודש הבא',
+  'Previous month': 'חודש קודם',
+  'Choose Date': 'בחר תאריך',
+}
