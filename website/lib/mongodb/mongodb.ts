d6ceefc4c7f1cd296177aa1c069995aeb73f7d7c
Tom Österlund <tom.osterlund1@gmail.com>
2025-07-03 07:42:29 +0200

    docs: wip

--- website/lib/mongodb/mongodb.ts
+++ website/lib/mongodb/mongodb.ts
@@ -12,6 +12,10 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined
 }
 
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local')
+}
+
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
