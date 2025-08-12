e26d770acbff70fa4673312902032017f8efaab5
Tom Österlund <tom.osterlund1@gmail.com>
2025-08-12 19:16:01 +0200

    feat: tz dropdown wip

--- packages/timezone-select/src/translations/index.ts
+++ packages/timezone-select/src/translations/index.ts
@@ -1,7 +1,13 @@
 import { timezoneEnUS } from './en-US'
 import { timezoneDeDE } from './de-DE'
+import { timezoneFrFR } from './fr-FR'
+import { timezoneEsES } from './es-ES'
+import { timezoneItIT } from './it-IT'
 
 export const translations = {
   enUS: timezoneEnUS,
   deDE: timezoneDeDE,
+  frFR: timezoneFrFR,
+  esES: timezoneEsES,
+  itIT: timezoneItIT,
 }
