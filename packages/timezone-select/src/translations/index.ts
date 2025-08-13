8c767411aaa4da5087da75bf33224fc928faa0c1
Tom Österlund <tom.osterlund1@gmail.com>
2025-08-13 09:58:02 +0200

    feat: tz dropdown wip

--- packages/timezone-select/src/translations/index.ts
+++ packages/timezone-select/src/translations/index.ts
@@ -9,6 +9,28 @@ import { timezoneTrTR } from './tr-TR'
 import { timezoneSvSE } from './sv-SE'
 import { timezoneHeIL } from './he-IL'
 import { timezoneJaJP } from './ja-JP'
+import { timezoneKoKR } from './ko-KR'
+import { timezonePlPL } from './pl-PL'
+import { timezoneArEG } from './ar-EG'
+import { timezoneCaES } from './ca-ES'
+import { timezoneCsCZ } from './cs-CZ'
+import { timezoneDaDK } from './da-DK'
+import { timezoneEnGB } from './en-GB'
+import { timezoneEtEE } from './et-EE'
+import { timezoneFiFI } from './fi-FI'
+import { timezoneFrCH } from './fr-CH'
+import { timezoneHrHR } from './hr-HR'
+import { timezonePtBR } from './pt-BR'
+import { timezoneUkUA } from './uk-UA'
+import { timezoneSrRS } from './sr-RS'
+import { timezoneSrLatnRS } from './sr-Latn-RS'
+import { timezoneSlSL } from './sl-SL'
+import { timezoneSkSK } from './sk-SK'
+import { timezoneRuRU } from './ru-RU'
+import { timezoneRoRO } from './ro-RO'
+import { timezoneNlNL } from './nl-NL'
+import { timezoneMkMK } from './mk-MK'
+import { timezoneLtLT } from './lt-LT'
 
 export const translations = {
   enUS: timezoneEnUS,
@@ -22,4 +44,26 @@ export const translations = {
   svSE: timezoneSvSE,
   heIL: timezoneHeIL,
   jaJP: timezoneJaJP,
+  koKR: timezoneKoKR,
+  plPL: timezonePlPL,
+  arEG: timezoneArEG,
+  caES: timezoneCaES,
+  csCZ: timezoneCsCZ,
+  daDK: timezoneDaDK,
+  enGB: timezoneEnGB,
+  etEE: timezoneEtEE,
+  fiFI: timezoneFiFI,
+  frCH: timezoneFrCH,
+  hrHR: timezoneHrHR,
+  ptBR: timezonePtBR,
+  ukUA: timezoneUkUA,
+  srRS: timezoneSrRS,
+  srLatnRS: timezoneSrLatnRS,
+  slSL: timezoneSlSL,
+  skSK: timezoneSkSK,
+  ruRU: timezoneRuRU,
+  roRO: timezoneRoRO,
+  nlNL: timezoneNlNL,
+  mkMK: timezoneMkMK,
+  ltLT: timezoneLtLT,
 }
