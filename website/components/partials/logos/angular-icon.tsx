e79ef71838dd89a87fd35fe4fec356825dc573c4
Anton Dettki <caspardett@gmail.com>
2025-08-26 19:31:43 +0200

    fix: add angular icon

---
+++ website/components/partials/logos/angular-icon.tsx
@@ -0,0 +1,38 @@
+export default function AngularIcon() {
+    return (
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 960 960"
+          width={40}
+          height={40}
+          style={{
+            display: 'inline',
+            position: 'relative',
+            top: '-3px',
+            marginRight: '5px',
+          }}
+        >
+          <g>
+            <polygon
+              points="562.6,109.8 804.1,629.5 829.2,233.1"
+              fill="#DD0031"
+            />
+            <polygon
+              points="396.6,109.8 130,233.1 155.1,629.5"
+              fill="#DD0031"
+            />
+            <polygon
+              points="624.9,655.9 334.3,655.9 297.2,745.8 479.6,849.8 662,745.8"
+              fill="#C3002F"
+            />
+            <polygon
+              points="384.1,539.3 575.2,539.3 479.6,307"
+              fill="#DD0031"
+            />
+            <polygon points="421,460 538,460 552,495 407,495" fill="#DD0031" />
+          </g>
+        </svg>
+      </>
+    )
+}
