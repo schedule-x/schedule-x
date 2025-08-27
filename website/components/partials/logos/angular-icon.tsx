b912a4b94cff5a8dbcc3ae49f74e08df0e56bce5
Anton Dettki <caspardett@gmail.com>
2025-08-27 19:02:22 +0200

    fix: add attribution

--- website/components/partials/logos/angular-icon.tsx
+++ website/components/partials/logos/angular-icon.tsx
@@ -2,36 +2,22 @@ export default function AngularIcon() {
     return (
       <>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 960 960"
-          width={40}
-          height={40}
           style={{
             display: 'inline',
             position: 'relative',
             top: '-3px',
             marginRight: '5px',
           }}
+          width={40}
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <g>
-            <polygon
-              points="562.6,109.8 804.1,629.5 829.2,233.1"
-              fill="#DD0031"
-            />
-            <polygon
-              points="396.6,109.8 130,233.1 155.1,629.5"
-              fill="#DD0031"
-            />
-            <polygon
-              points="624.9,655.9 334.3,655.9 297.2,745.8 479.6,849.8 662,745.8"
-              fill="#C3002F"
-            />
-            <polygon
-              points="384.1,539.3 575.2,539.3 479.6,307"
-              fill="#DD0031"
-            />
-            <polygon points="421,460 538,460 552,495 407,495" fill="#DD0031" />
-          </g>
+          <path d="M16 2L3 7L5 24L16 30L27 24L29 7L16 2Z" fill="#DD0031" />
+          <path d="M16 2V30L27 24L29 7L16 2Z" fill="#C3002F" />
+          <path
+            d="M15.9998 5.09375L7.87305 23.3638H10.9031L12.5368 19.2757H19.4348L21.0685 23.3638H24.0986L15.9998 5.09375ZM18.3736 16.7557H13.626L15.9998 11.0298L18.3736 16.7557Z"
+            fill="white"
+          />
         </svg>
       </>
     )
