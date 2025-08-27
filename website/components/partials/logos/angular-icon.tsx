bd2c857cf160e56896976585f78f00a36316b53d
Anton <102290892+Anton-Dettki@users.noreply.github.com>
2025-08-27 22:26:21 +0200

    docs: add angular icon (#1159)

---
+++ website/components/partials/logos/angular-icon.tsx
@@ -0,0 +1,24 @@
+export default function AngularIcon() {
+    return (
+      <>
+        <svg
+          style={{
+            display: 'inline',
+            position: 'relative',
+            top: '-3px',
+            marginRight: '5px',
+          }}
+          width={40}
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M16 2L3 7L5 24L16 30L27 24L29 7L16 2Z" fill="#DD0031" />
+          <path d="M16 2V30L27 24L29 7L16 2Z" fill="#C3002F" />
+          <path
+            d="M15.9998 5.09375L7.87305 23.3638H10.9031L12.5368 19.2757H19.4348L21.0685 23.3638H24.0986L15.9998 5.09375ZM18.3736 16.7557H13.626L15.9998 11.0298L18.3736 16.7557Z"
+            fill="white"
+          />
+        </svg>
+      </>
+    )
+}
