a7650f4817887d302c41ffd00f9e7713f869b423
Tom Österlund <tom.osterlund1@gmail.com>
2026-01-14 14:29:17 +0100

    chore(shared): expose new apis

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,6 +4,15 @@ import type { CalendarEventInternal } from './interfaces/calendar/calendar-event
 import type EventModalPlugin from './interfaces/event-modal/event-modal.plugin'
 import type TimezoneSelectPlugin from './interfaces/timezone-select/timezone-select-plugin'
 import type CalendarConfigInternal from './interfaces/calendar/calendar-config'
+import type CalendarState from './interfaces/calendar/calendar-state.interface'
+import type CalendarEvents from './interfaces/calendar/calendar-events.interface'
+import type DragAndDropPlugin from './interfaces/drag-and-drop/drag-and-drop-plugin.interface'
+import type TimeGridDragHandler from './interfaces/drag-and-drop/time-grid-drag-handler.interface'
+import type DateGridDragHandler from './interfaces/drag-and-drop/date-grid-drag-handler.interface'
+import type MonthGridDragHandler from './interfaces/drag-and-drop/month-grid-drag-handler.interface'
+import type DragHandlerDependencies from './interfaces/drag-and-drop/drag-handler-dependencies.interface'
+import type { ResizePlugin } from './interfaces/resize/resize-plugin.interface'
+import type { EventCoordinates } from './interfaces/shared/event-coordinates'
 import TimeIcon from './components/icons/time-icon'
 import UserIcon from './components/icons/user-icon'
 import DescriptionIcon from './components/icons/description-icon'
@@ -12,6 +21,10 @@ import PluginBase from './interfaces/plugin.interface'
 
 export { definePlugin } from './utils/stateless/calendar/define-plugin'
 export { deepCloneEvent } from './utils/stateless/calendar/deep-clone-event'
+export { getTimePointsPerPixel } from './utils/stateless/calendar/time-points-per-pixel'
+export { getTimeGridDayWidth } from './utils/stateless/calendar/get-time-grid-day-width'
+export { getTimeGridEventCopyElementId } from './utils/stateless/strings/selector-generators'
+export { getEventCoordinates } from './utils/stateless/dom/get-event-coordinates'
 export { concatenatePeople } from './utils/stateless/strings/concatenate-people'
 export {
   getTimeStamp,
@@ -48,6 +61,15 @@ export type {
 }
 export type { View, ViewConfig } from './types/calendar/view'
 export type { CalendarAppSingleton, PluginBase }
+export type { CalendarState }
+export type { CalendarEvents }
+export type { DragAndDropPlugin }
+export type { TimeGridDragHandler }
+export type { DateGridDragHandler }
+export type { MonthGridDragHandler }
+export type { DragHandlerDependencies }
+export type { ResizePlugin }
+export type { EventCoordinates }
 export type { CustomComponentFns as CustomComponents } from './interfaces/calendar/custom-component-fns'
 export type { CustomComponentName } from './interfaces/calendar/custom-component-fns'
 export {
@@ -55,6 +77,8 @@ export {
   addMonths,
   __deprecated__addMinutes as addMinutes,
 } from './utils/stateless/time/date-time-mutation/adding'
+export { setDateInDateTime } from './utils/stateless/time/date-time-mutation/date-time-mutation'
+export { calculateDaysDifference } from './utils/stateless/time/days-difference'
 export {
   timeStringFromTimePoints,
   addTimePointsToDateTime,
@@ -69,6 +93,7 @@ export type { DayBoundariesDateTime } from './types/day-boundaries-date-time'
 export type { DayBoundariesInternal } from './types/calendar/day-boundaries'
 export type { RangeSetterConfig } from './interfaces/calendar/range-setter-config.interface'
 export type { PreactViewComponent } from './types/calendar/preact-view-component'
+export { PluginName } from './enums/plugin-name.enum'
 
 // Preact utilities for views
 export { AppContext } from './utils/stateful/app-context'
