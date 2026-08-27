0e2adbe1d3eb47c10420e132de9bfbfeae92142f
Tom Österlund <tom.osterlund1@gmail.com>
2026-08-27 16:12:32 +0200

    feat(calendar): support view-specific headers (#1351)
    Co-authored-by: Tom Österlund <tom@schedule-x.dev>

--- packages/calendar/src/components/header/__test__/calendar-header/utils.tsx
+++ packages/calendar/src/components/header/__test__/calendar-header/utils.tsx
@@ -1,12 +1,12 @@
 import CalendarAppSingleton from '@schedule-x/shared/src/interfaces/calendar/calendar-app-singleton'
 import { render } from '@testing-library/preact'
 import { AppContext } from '../../../../utils/stateful/app-context'
-import CalendarHeader from '../../calendar-header'
+import DefaultHeader from '../../default-header'
 
 export const renderComponent = (app: CalendarAppSingleton) => {
   render(
     <AppContext.Provider value={app}>
-      <CalendarHeader />
+      <DefaultHeader />
     </AppContext.Provider>
   )
 }
