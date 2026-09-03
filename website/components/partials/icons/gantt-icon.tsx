4db9b4c558ae33932d20347399b2a1236d9f8782
Tom Österlund <tom@schedule-x.dev>
2026-09-03 12:58:13 +0000

    docs: enrich Gantt demo project

--- website/components/partials/icons/gantt-icon.tsx
+++ website/components/partials/icons/gantt-icon.tsx
@@ -7,12 +7,12 @@ export default function GanttIcon() {
       viewBox="0 0 48 48"
       fill="none"
     >
-      <rect x="5" y="8" width="18" height="7" rx="3.5" fill="#6750A4" />
-      <rect x="16" y="21" width="25" height="7" rx="3.5" fill="#7D5260" />
-      <rect x="9" y="34" width="20" height="7" rx="3.5" fill="#006A6A" />
+      <rect x="5" y="8" width="18" height="7" rx="3.5" fill="#000" />
+      <rect x="16" y="21" width="25" height="7" rx="3.5" fill="#000" />
+      <rect x="9" y="34" width="20" height="7" rx="3.5" fill="#000" />
       <path
         d="M23 11.5h5v13h-2M29 37.5h6v-13h6"
-        stroke="#49454F"
+        stroke="#000"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
